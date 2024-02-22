78cedec3d64f37edf1ccf3c43dd6115d93e2e655
roshan9888 <100696071+roshan9888@users.noreply.github.com>
2024-02-23 02:08:46 +0530

    Time: 106 ms (31.23%) | Memory: 52.3 MB (8.16%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int count=0;
-        for(int i=0;i<nums.size();i++){   
-            int sum=0;  
-            // k=k-nums[i];
-            for(int j=i;j<nums.size();j++){
-                // k=k-nums[j];
-                sum+=nums[j];
-                if(sum==k){
-                    count++;
-                    // break;
-                }
-            }
+        //brute force solution
+        // int count=0;
+        // for(int i=0;i<nums.size();i++){   
+        //     int sum=0;  
+        //     for(int j=i;j<nums.size();j++){
+        //         sum+=nums[j];
+        //         if(sum==k){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+
+        //optimal solution
+        map<int,int>mp;
+        mp[0]=1;
+        int cnt=0,presum=0;
+        for(int i=0;i<nums.size();i++){
+            presum+=nums[i];
+            int remove=presum-k;
+            cnt+=mp[remove];
+            mp[presum]+=1;
         }
-        return count;
+        return cnt;
     }
 };
 
-// class Solution {
-// public:
-//     int subarraySum(vector<int>& nums, int k) {
-//         int count = 0;
-//         int n = nums.size();
-//         for (int start = 0; start < n; ++start) {
-//             int sum = 0;
-//             for (int end = start; end < n; ++end) {
-//                 sum += nums[end];
-//                 if (sum == k) {
-//                     count++;
-//                 }
-//             }
-//         }
-//         return count;
-//     }
-// };
