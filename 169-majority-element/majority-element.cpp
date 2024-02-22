ca4357a86e68cd070314b825ae8fade06db6843d
roshan9888 <100696071+roshan9888@users.noreply.github.com>
2024-02-23 03:47:32 +0530

    Time: 21 ms (12.16%) | Memory: 26.7 MB (5.16%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        //brute force solution
+        // int n=nums.size();
+        // int m=floor(n/2);
+        // cout<<m<<" ";
+        // for(int i=0;i<n;i++){
+        //     int count=0;
+        //     int occur=nums[i];
+        //     for(int j=0;j<n;j++){
+        //         if(nums[j]==occur){
+        //             count++;
+        //         }
+        //     }
+        //     if(count>m){
+        //         cout<<count<<" ";
+        //         return occur;
+        //     }
+        // }
+        // return -1;
+
+        //optimal solution
         int n=nums.size();
         int m=floor(n/2);
-        cout<<m<<" ";
+        map<int,int>mp;
         for(int i=0;i<n;i++){
-            int count=0;
-            int occur=nums[i];
-            for(int j=0;j<n;j++){
-                if(nums[j]==occur){
-                    count++;
-                }
-            }
-            if(count>m){
-                cout<<count<<" ";
-                return occur;
+            mp[nums[i]]++;
+        }
+
+        for(auto it:mp){
+            if(it.second>m){
+                return it.first;
             }
         }
         return -1;
