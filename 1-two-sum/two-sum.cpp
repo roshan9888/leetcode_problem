a3eb81bd60f945327d0348e503ec6903da0a5886
roshan9888 <100696071+roshan9888@users.noreply.github.com>
2024-02-23 02:35:43 +0530

    Time: 51 ms (39.86%) | Memory: 12.7 MB (42.03%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,30 +3,46 @@ bool cmp(pair<int,int>a,pair<int,int>b){
 }
 class Solution {
 public:
+    
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int,int>>data;
-        for(int i=0;i<nums.size();i++){
-            int value=nums[i];
-            int index=i;
-            data.push_back({value,index});
-        }
-        sort(data.begin(),data.end(),cmp);
-    int start=0;
-    int end=nums.size()-1;
-    vector<int>ans;
-    while(start<end){
-        if(data[start].first + data[end].first == target){
-            ans.push_back(data[start].second);
-            ans.push_back(data[end].second);
-            break;
-        }
-        else if(data[start].first + data[end].first < target){
-            start++;
-        }
-        else{
-            end--;
-        }
-    }
-    return ans;
+      //brute force solution
+
+      for(int i=0;i<nums.size()-1;i++){
+          for(int j=i+1;j<nums.size();j++){
+              if(nums[i]==target-nums[j]){
+                  return {i,j};
+              }
+          }
+      }
+
+      return {0,0};
+
+
+
+         //optimal solution
+    //     vector<pair<int,int>>data;
+    //     for(int i=0;i<nums.size();i++){
+    //         int value=nums[i];
+    //         int index=i;
+    //         data.push_back({value,index});
+    //     }
+    //     sort(data.begin(),data.end(),cmp);
+    // int start=0;
+    // int end=nums.size()-1;
+    // vector<int>ans;
+    // while(start<end){
+    //     if(data[start].first + data[end].first == target){
+    //         ans.push_back(data[start].second);
+    //         ans.push_back(data[end].second);
+    //         break;
+    //     }
+    //     else if(data[start].first + data[end].first < target){
+    //         start++;
+    //     }
+    //     else{
+    //         end--;
+    //     }
+    // }
+    // return ans;
     }
 };
