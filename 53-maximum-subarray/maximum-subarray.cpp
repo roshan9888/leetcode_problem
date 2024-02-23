9ecda0ef21aad697e65ea034980ff3a2006f0a46
roshan9888 <100696071+roshan9888@users.noreply.github.com>
2024-02-23 17:10:47 +0530

    Time: 75 ms (80.68%) | Memory: 70.2 MB (19.65%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -13,51 +13,51 @@ public:
         // return maxsum;
 
         //optimal solution
-        // int i=0;
-        // int maxsum=nums[i];
-        // int sum=0;
-        // vector<int>ans;
-        // while(i<nums.size()){
-        //     sum+=nums[i];
-        //     maxsum=max(sum,maxsum);
-        //     if(sum<0){
-        //         sum=0;
-        //     }
-        //     i++;
-        // }
-        // for(auto it:ans){
-        //     cout<<it<<" ";
-        // }
-        // return maxsum;
-
-        //for storing values 
-        vector<int>ans;
         int i=0;
         int maxsum=nums[i];
         int sum=0;
-        int start=0;
-        int end=0;
-        int tempstart=0;
+        vector<int>ans;
         while(i<nums.size()){
             sum+=nums[i];
-            if(sum>maxsum){
-                maxsum=sum;
-                start=tempstart;
-                end=i;
-            }
+            maxsum=max(sum,maxsum);
             if(sum<0){
                 sum=0;
-                tempstart=i+1;
             }
             i++;
-        }
-
-        for(int i=start;i<=end;i++){
-            ans.push_back(nums[i]);
         }
         for(auto it:ans){
             cout<<it<<" ";
         }
         return maxsum;
+
+        //for storing values 
+        // vector<int>ans;
+        // int i=0;
+        // int maxsum=nums[i];
+        // int sum=0;
+        // int start=0;
+        // int end=0;
+        // int tempstart=0;
+        // while(i<nums.size()){
+        //     sum+=nums[i];
+        //     if(sum>maxsum){
+        //         maxsum=sum;
+        //         start=tempstart;
+        //         end=i;
+        //     }
+        //     if(sum<0){
+        //         sum=0;
+        //         tempstart=i+1;
+        //     }
+        //     i++;
+        // }
+
+        // for(int i=start;i<=end;i++){
+        //     ans.push_back(nums[i]);
+        // }
+        // for(auto it:ans){
+        //     cout<<it<<" ";
+        // }
+        // return maxsum;
     }
 };
