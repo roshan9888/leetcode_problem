db075753b129ad772415581d2131b7c046160d9c
roshan9888 <100696071+roshan9888@users.noreply.github.com>
2024-02-24 23:33:53 +0530

    Time: 135 ms (56.13%) | Memory: 74.1 MB (19.08%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -6,21 +6,43 @@ public:
         // time complexity O(n^2)
 
         //better solution
-        if(nums.size()==0) return 0;
-        sort(nums.begin(),nums.end());
-        int count=0;
+        // if(nums.size()==0) return 0;
+        // sort(nums.begin(),nums.end());
+        // int count=0;
+        // int longest=1;
+        // int lastsmaller=INT_MIN;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]-1==lastsmaller){
+        //         count++;
+        //         lastsmaller=nums[i];
+        //     }
+        //     else if(nums[i]!=lastsmaller){
+        //         count=1;
+        //         lastsmaller=nums[i];
+        //     }
+        //     longest=max(longest,count);
+        // }
+        // return longest;
+
+        //optimal solution
+        int n=nums.size();
+        if(n==0) return 0;
         int longest=1;
-        int lastsmaller=INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]-1==lastsmaller){
-                count++;
-                lastsmaller=nums[i];
+        unordered_set<int>st;
+        for(int i=0;i<n;i++){
+            st.insert(nums[i]);
+        }
+
+        for(auto it:st){
+            if(st.find(it-1)==st.end()){
+                int cnt=1;
+                int x=it;
+                while(st.find(x+1)!=st.end()){
+                    x=x+1;
+                    cnt++;
+                }
+                longest=max(longest,cnt);
             }
-            else if(nums[i]!=lastsmaller){
-                count=1;
-                lastsmaller=nums[i];
-            }
-            longest=max(longest,count);
         }
         return longest;
     }
