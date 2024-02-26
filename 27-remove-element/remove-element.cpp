bebf39b3017adb552c38a08907234d873e144a11
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-02-26 20:48:21 +0530

    Time: 3 ms (34.24%) | Memory: 10.5 MB (23.82%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,19 +1,49 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int xor1=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==val){
-               nums[i]=INT_MAX;
+        // int xor1=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==val){
+        //        nums[i]=INT_MAX;
+        //     }
+        // }
+        // int count=0;
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]!=INT_MAX){
+        //         count++;
+        //     }
+        // }
+        // return count;
+
+        int i=0;
+        int j=nums.size()-1;
+        while(i<j){
+            if(nums[i]==val && nums[j]==val){
+                j--;
+            }
+            else if(nums[i]==val && nums[j]!=val){
+                cout<<nums[i]<<"swap"<<nums[j]<<" ";
+                swap(nums[i],nums[j]);
+                i++;
+                j--;
+            }
+            else if(nums[i]!=val && nums[j]==val){
+            //    cout<<nums[i]<<"swap"<<nums[j]<<" ";
+               i++;
+               j--;
+            }
+            else{
+            i++;
             }
         }
         int count=0;
-        sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++){
-            if(nums[i]!=INT_MAX){
+            if(nums[i]!=val){
                 count++;
             }
         }
+        cout<<count<<" ";
         return count;
     }
 };
