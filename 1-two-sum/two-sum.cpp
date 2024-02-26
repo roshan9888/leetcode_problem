9ed3628adaffe74599cecea7baed94cba9f51926
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-02-26 22:24:41 +0530

    Time: 8 ms (81.34%) | Memory: 13.4 MB (29.98%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -7,42 +7,42 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
       //brute force solution
 
-      for(int i=0;i<nums.size()-1;i++){
-          for(int j=i+1;j<nums.size();j++){
-              if(nums[i]==target-nums[j]){
-                  return {i,j};
-              }
-          }
-      }
+    //   for(int i=0;i<nums.size()-1;i++){
+    //       for(int j=i+1;j<nums.size();j++){
+    //           if(nums[i]==target-nums[j]){
+    //               return {i,j};
+    //           }
+    //       }
+    //   }
 
-      return {0,0};
+    //   return {0,0};
 
 
 
          //optimal solution
-    //     vector<pair<int,int>>data;
-    //     for(int i=0;i<nums.size();i++){
-    //         int value=nums[i];
-    //         int index=i;
-    //         data.push_back({value,index});
-    //     }
-    //     sort(data.begin(),data.end(),cmp);
-    // int start=0;
-    // int end=nums.size()-1;
-    // vector<int>ans;
-    // while(start<end){
-    //     if(data[start].first + data[end].first == target){
-    //         ans.push_back(data[start].second);
-    //         ans.push_back(data[end].second);
-    //         break;
-    //     }
-    //     else if(data[start].first + data[end].first < target){
-    //         start++;
-    //     }
-    //     else{
-    //         end--;
-    //     }
-    // }
-    // return ans;
+        vector<pair<int,int>>data;
+        for(int i=0;i<nums.size();i++){
+            int value=nums[i];
+            int index=i;
+            data.push_back({value,index});
+        }
+        sort(data.begin(),data.end(),cmp);
+    int start=0;
+    int end=nums.size()-1;
+    vector<int>ans;
+    while(start<end){
+        if(data[start].first + data[end].first == target){
+            ans.push_back(data[start].second);
+            ans.push_back(data[end].second);
+            break;
+        }
+        else if(data[start].first + data[end].first < target){
+            start++;
+        }
+        else{
+            end--;
+        }
+    }
+    return ans;
     }
 };
