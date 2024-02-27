8643ae7e31a2ba996db08d62a473a63c6e51ae48
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-02-28 02:19:15 +0530

    Time: 343 ms (28.57%) | Memory: 125.1 MB (35.90%) - LeetSync

--- 2656-count-the-number-of-beautiful-subarrays/count-the-number-of-beautiful-subarrays.cpp
+++ 2656-count-the-number-of-beautiful-subarrays/count-the-number-of-beautiful-subarrays.cpp
@@ -1,41 +1,7 @@
 class Solution {
 public:
     long long beautifulSubarrays(vector<int>& nums) {
-        //brute force
-        // int count=0;
-        // for(int i=0;i<nums.size();i++){
-        //     for(int j=i;j<nums.size();j++){
-        //         int x=0;
-        //         for(int k=i;k<=j;k++){
-        //             x=x^nums[k];
-        //         }
-        //         if(x==0){
-        //         count++;
-        //         }
-        //     }
-        // }
-        // return count;
-
-        //time complexity O(n^3)
-
-
-        //better solution
-        // int count =0;
-        // for(int i=0;i<nums.size();i++){
-        //     int x1=0;
-        //     for(int j=i;j<nums.size();j++){
-        //         x1=x1^nums[j];
-        //         if(x1==0){
-        //             count++;
-        //         }
-        //     }
-        // }
-        // return count;  
-
-        //time complexity O(n^2)
-
-
-        //optimal solution
+        
         map<int,int>mp;
         int k=0;
         mp[0]=1;
