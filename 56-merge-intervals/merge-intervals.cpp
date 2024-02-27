2e34b1353c505d12ce088f3d1b325ccfd3292fe9
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-02-28 04:20:11 +0530

    Time: 23 ms (74.44%) | Memory: 22.7 MB (45.98%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,29 +1,51 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
-        vector<vector<int>> ans;
+        //brute force
+    //     vector<vector<int>> ans;
+    //     sort(arr.begin(),arr.end());
+    //     int n=arr.size();
+    //     for (int i = 0; i < n; i++) { // select an interval:
+    //     int start = arr[i][0];
+    //     int end = arr[i][1];
+
+    //     //Skip all the merged intervals:
+    //     if (!ans.empty() && end <= ans.back()[1]) {
+    //         continue;
+    //     }
+
+    //     //check the rest of the intervals:
+    //     for (int j = i + 1; j < n; j++) {
+    //         if (arr[j][0] <= end) {
+    //             end = max(end, arr[j][1]);
+    //         }
+    //         else {
+    //             break;
+    //         }
+    //     }
+    //     ans.push_back({start, end});
+    // }
+    // return ans;
+
+    // time complexity O(nlogn + n^2)
+
+    //optimal solution
+    int n = arr.size();
         sort(arr.begin(),arr.end());
-        int n=arr.size();
-        for (int i = 0; i < n; i++) { // select an interval:
-        int start = arr[i][0];
-        int end = arr[i][1];
-
-        //Skip all the merged intervals:
-        if (!ans.empty() && end <= ans.back()[1]) {
-            continue;
-        }
-
-        //check the rest of the intervals:
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j][0] <= end) {
-                end = max(end, arr[j][1]);
+        vector<vector<int>>ans;
+        for(int i = 0 ;i < n;i++)
+        {
+            if(ans.empty() ||  arr[i][0] > ans.back()[1])
+            {
+                ans.push_back(arr[i]);
             }
-            else {
-                break;
+            else
+            {
+                ans.back()[1] = max(arr[i][1] , ans.back()[1]);
             }
-        }
-        ans.push_back({start, end});
-    }
-    return ans;
+        } 
+        return ans ;
+
+        //time complexity O(n)
     }
 };
