02cbe809b493b091bf4183d61fd44e2821c3e27b
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-02-29 03:39:15 +0530

    Time: 22 ms (72.99%) | Memory: 26.7 MB (60.40%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,28 +1,53 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        map<int,int>mp;
-        int n=grid.size();
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid.size();j++){
-                mp[grid[i][j]]++;
+        // map<int,int>mp;
+        // int n=grid.size();
+        // for(int i=0;i<grid.size();i++){
+        //     for(int j=0;j<grid.size();j++){
+        //         mp[grid[i][j]]++;
+        //     }
+        // }
+        // for(auto it:mp){
+        //     cout<<it.first<<" "<<it.second<<endl;
+        // }
+        // vector<int>ans;
+        // for(auto it:mp){
+        //    if(it.second>=2){
+        //        ans.push_back(it.first);
+        //    }
+        // }
+        // for(int i=1;i<=n*n;i++){
+        //    if(mp.find(i)==mp.end()){
+        //     ans.push_back(i);
+        // }
+        // }
+        // return ans;
+
+        //without using map
+        int n = grid.size(),sum=0;
+        vector<int> s;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<grid[i].size();j++)
+            {
+                sum+=grid[i][j];
+                s.push_back(grid[i][j]);
             }
         }
-        for(auto it:mp){
-            cout<<it.first<<" "<<it.second<<endl;
+        sort(s.begin(),s.end());
+        vector<int> ans(2);
+        for(int i=0;i<s.size();i++)
+        {
+            if(i>0&&s[i]==s[i-1])
+            {
+                ans[0]=s[i-1];
+            }
         }
-        vector<int>ans;
-        for(auto it:mp){
-           if(it.second>=2){
-               ans.push_back(it.first);
-           }
-        }
-        for(int i=1;i<=n*n;i++){
-           if(mp.find(i)==mp.end()){
-            ans.push_back(i);
-        }
-        }
+        int m=n*n,count=(m*(m+1))/2;
         
+        ans[1]=count-(sum-ans[0]);
+       
         return ans;
     }
 };
