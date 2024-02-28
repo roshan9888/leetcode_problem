5c86f9e5752676f91874cc3af21dd671d2d27a37
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-02-29 00:33:08 +0530

    Time: 124 ms (61.88%) | Memory: 80.7 MB (56.34%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -13,28 +13,47 @@ public:
 
         // time complexity O(n^2)
 
-        //optimal solution
-        unordered_map<int,vector<int>>indexMap;
-        for(int i=0;i<nums.size();i++){
-            indexMap[nums[i]].push_back(i);
-        }
+        //better solution
+        // unordered_map<int,vector<int>>indexMap;
+        // for(int i=0;i<nums.size();i++){
+        //     indexMap[nums[i]].push_back(i);
+        // }
 
-        for(auto it:indexMap){
-            if(it.second.size() ==2  && abs(it.second[0]-it.second[1])<=k){
-              return true;
+        // for(auto it:indexMap){
+        //     if(it.second.size() == 2  && abs(it.second[0]-it.second[1])<=k){
+        //       return true;
+        //     }
+        //     else if(it.second.size()>2){
+        //         for(int i=0;i<it.second.size()-1;i++){
+        //             for(int j=i+1;j<it.second.size();j++){
+        //                 cout<<abs(it.second[i]-it.second[j])<<" ";
+        //                 if(abs(it.second[i]-it.second[j])<=k){
+        //                     return true;
+        //                 }
+        //             }
+        //         }
+        //         return false;
+        //     }
+        // }
+        // return false;
+
+        //optimal solution
+        unordered_map<int,int> mp;
+        int n = nums.size();
+        for(int i=0; i<n; i++)
+        {
+            // mp.count() will tell whatever ith index that I want, have I seen it before?
+            if(mp.count(nums[i]))
+            {
+                // if I have already seen this number, then check for condition abs(i - j) <= k
+                if(abs(i-mp[nums[i]])<=k)
+                    return true;
             }
-            else if(it.second.size()>2){
-                for(int i=0;i<it.second.size()-1;i++){
-                    for(int j=i+1;j<it.second.size();j++){
-                        cout<<abs(it.second[i]-it.second[j])<<" ";
-                        if(abs(it.second[i]-it.second[j])<=k){
-                            return true;
-                        }
-                    }
-                }
-                return false;
-            }
+            // if I have not seen this number before, insert the number with its position in the map
+            // and if the number is already present in the map, then update the position of that number
+            mp[nums[i]] = i;
         }
+        // after the complete traversal, if we don't find a pair to satisfy the condition, return false
         return false;
     }
 };
