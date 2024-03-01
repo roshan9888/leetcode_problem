ad2b4a081913fa24cbdd0962cc30899e6cf8d838
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-03-02 02:54:25 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (30.29%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n=nums.size();
-        if(nums.size()==2){
-            if(nums[1]>nums[0]){
-                return 1;
-            }
-        }
-        // int n = nums.size();
-        if (n == 1) return 0; // If there's only one element, it's a peak.
+        //brute force
+        // int n=nums.size();
+        // if (n == 1) return 0;
 
-        for (int i = 1; i < n - 1; i++) {
-            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
-                return i; // Peak found.
+        // for (int i = 1; i < n - 1; i++) {
+        //     if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+        //         return i;
+        //     }
+        // }
+        // if (nums[0] > nums[1]) return 0; 
+        // if (nums[n - 1] > nums[n - 2]) return n - 1;
+        
+        // return -1; 
+
+        //optimal solution
+        int n=nums.size();
+        if(nums.size()==1) return 0;
+        int low=1;
+        int high=n-1;
+        if (nums[0] > nums[1]) return 0; 
+        if (nums[n - 1] > nums[n - 2]) return n - 1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            if(nums[mid]>nums[mid-1] && nums[mid]<nums[mid+1]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
             }
         }
-        // Check if the first or last element is a peak.
-        if (nums[0] > nums[1]) return 0; // Peak at the beginning.
-        if (nums[n - 1] > nums[n - 2]) return n - 1; // Peak at the end.
-        
-        return -1; // No peak found.
+        return -1;
     }
 };
