a401e763d53cb795f40a011b25d7080a5a8be224
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-03-01 17:20:12 +0530

    Time: 3 ms (81.30%) | Memory: 16.5 MB (48.52%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,62 +1,34 @@
 class Solution {
 public:
-    int findPivot(vector<int>& nums) {
-        int start = 0;
-        int end = nums.size() - 1;
-
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-
-            if (mid < end && nums[mid] > nums[mid + 1]) {
-                return mid;
-            }
-            if (mid > start && nums[mid] < nums[mid - 1]) {
-                return mid - 1;
-            }
-
-            if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
-                if (nums[start] > nums[start + 1]) {
-                    return start;
-                }
-                start++;
-                if (nums[end] < nums[end - 1]) {
-                    return end - 1;
-                }
-                end--;
-            } else if (nums[start] < nums[mid] || (nums[start] == nums[mid] && nums[mid] > nums[end])) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return start;
-    }
-
-    int binarySearch(vector<int>& nums, int start, int end, int target) {
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return -1;
-    }
-
     bool search(vector<int>& nums, int target) {
-        int pivotIndex = findPivot(nums);
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
 
-        if (nums[pivotIndex] == target) {
-            return true;
+            if(nums[mid]==target) return true;
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && nums[mid]>=target){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && nums[high]>=target){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
         }
-
-        int left = binarySearch(nums, 0, pivotIndex, target);
-        int right = binarySearch(nums, pivotIndex + 1, nums.size() - 1, target);
-
-        return left != -1 || right != -1;
+        return false;
     }
 };
