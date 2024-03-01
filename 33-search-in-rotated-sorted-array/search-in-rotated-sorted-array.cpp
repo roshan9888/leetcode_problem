53570dc0becd92fbfa08b256e553cb265b2f28a2
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-03-01 17:04:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (25.15%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,58 +1,30 @@
 class Solution {
 public:
-int pivot(vector<int> arr){
-    int s=0;
-    int e=arr.size()-1;
-    int mid=s+(e-s)/2;
-    while(s<=e){
-        if(s==e){
-            return s;
-        }
-        if(mid+1<=e && arr[mid]>arr[mid+1]){
-            return mid;
-        }
-        if(mid-1>=s && arr[mid-1]>arr[mid]){
-            return mid-1;
-        }
-        if(arr[s]>arr[mid]){
-            e=mid-1;
-        }
-        else{
-            s=mid+1;
-        }
-        mid=s+(e-s)/2;
-    }
-    return -1;
-}
-  int binarysearch(vector<int> arr,int start,int end,int target){
-    int mid = start+(end-start) / 2;
-    while (start <= end)
-    {
-    int element=arr[mid];
-        if (target == element)
-        {
-            return mid;
-        }
-        else if(target<element){
-            end=mid-1;
-        }
-        else{
-            start=mid+1;
-        }
-         
-        mid = start+(end-start) / 2;
-    }
-    return -1;
-  }
     int search(vector<int>& nums, int target) {
-        int pivotindex=pivot(nums);
-        int ans=0;
-        if(target>=nums[0] && target<=nums[pivotindex]){
-            ans=binarysearch(nums,0,pivotindex,target);
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(nums[mid]==target) return mid;
+
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && nums[mid]>=target){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && nums[high]>=target){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
         }
-        else{
-            ans=binarysearch(nums,pivotindex+1,nums.size()-1,target);
-        }
-        return ans;
+        return -1;
     }
 };
