6e6551f77eeb24b32992c3f4a137cbf4d59064cc
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-03-03 01:54:08 +0530

    Time: 3 ms (74.29%) | Memory: 12 MB (31.08%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -16,27 +16,25 @@ public:
         // }
 
         //brute force
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]<=k) k++;
+        // for(int i=0;i<arr.size();i++){
+        //     if(arr[i]<=k) k++;
+        //     else{
+        //         break;
+        //     }
+        // }
+        // return k;
+        int n=arr.size();
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int missing=arr[mid]-(mid+1);
+            if(missing<k){
+                low=mid+1;
+            }
             else{
-                break;
+                high=mid-1;
             }
         }
-        return k;
-        // bool func(vector<int>arr,int mid){
-            
-        // }
-        //  int maxi=INT_MIN;
-        //  for(int i=0;i<arr.size();i++){
-        //      maxi=max(maxi,arr[i]);
-        //  }
-        // int low=1;
-        // int high=maxi;
-        // while(low<=high){
-        //     int mid=low+(high-low)/2;
-
-        //     int midN=func(arr,mid);
-        // }
-
+        return k+high+1;
     }
 };
