e6be9db7289a93ad21ef62ef22a21b2044c1d755
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-03-03 22:15:16 +0530

    Time: 7 ms (8.31%) | Memory: 9.1 MB (25.53%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int func(vector<int>&weights,int mid){
+        int capacity=0;
+        int count=1;
+        for(int i=0;i<weights.size();i++){
+            if(capacity+weights[i]>mid){
+               cout<<count<<" ";
+               count++;
+               capacity=weights[i];
+            }
+            else{
+                capacity+=weights[i];
+            }
+        }
+        return count;
+    }
+    int splitArray(vector<int>& weights, int days) {
+        int sum=0;
+        int maxi=INT_MIN;
+        for(int i=0;i<weights.size();i++){
+            sum+=weights[i];
+            maxi=max(maxi,weights[i]);
+        }
+        int low=maxi;
+        int high=sum;
+        while(low<=high){
+            int mid=(low+high)/2;
+
+            int midn=func(weights,mid);
+            // if(midn==days) return mid;
+            if(midn<=days){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+};
