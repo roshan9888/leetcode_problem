3b49685009525f75a26a4afe26d523cbd64ca3a5
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-03-03 06:48:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (11.04%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        stack<char>st;
+        vector<string>v;
+        string temp="";
+        for(int i=0;i<s.size();i++){
+            if(st.empty()){
+                st.push(s[i]);
+                temp+=s[i];
+            }
+            else if(st.top()=='(' && s[i]==')'){
+                temp+=s[i];
+                st.pop();
+            }
+            else{
+                temp+=s[i];
+                st.push(s[i]);
+            }
+            if(st.empty()){
+                v.push_back(temp);
+                temp="";
+            }
+        }
+        string sub="";
+        for (const auto& str : v) {
+        if (str.length() >= 2) {  // Ensure the string has at least two characters
+            sub += str.substr(1, str.length() - 2);
+            // cout << "Substring without first and last character of " << str << ": " << sub << endl;
+        } else {
+            // cout << "String \"" << str << "\" has less than 2 characters." << endl;
+            sub+=str;
+        }
+    }
+        return sub;
+    }
+};
