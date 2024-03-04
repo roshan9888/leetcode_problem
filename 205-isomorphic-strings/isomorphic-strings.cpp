e16ad750749939d8f6027944549fa0b0ae7f1b20
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-03-04 08:55:13 +0530

    Time: 6 ms (64.50%) | Memory: 8.1 MB (70.47%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+      int hash[256]={0};
+      bool istmapped[256]={0};
+
+      for(int i=0;i<s.size();i++){
+          if(hash[s[i]]==0 && istmapped[t[i]]==0){
+              hash[s[i]]=t[i];
+              istmapped[t[i]]=true;
+          }
+      }
+      for(int i=0;i<s.size();i++){
+          if(char(hash[s[i]])!=t[i]){
+              return false;
+          }
+      } 
+      return true;
+    }
+};
