55975340a177c815115578081df92d5c40420917
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-03-18 23:54:53 +0530

    Time: 186 ms (63.02%) | Memory: 144.6 MB (92.90%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -12,10 +12,35 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if(!root) return 0;
-        if(!root->left) return minDepth(root->right)+1;
-        if(!root->right) return minDepth(root->left)+1;
+        // if(!root) return 0;
+        // if(!root->left) return minDepth(root->right)+1;
+        // if(!root->right) return minDepth(root->left)+1;
 
-        return min(minDepth(root->left),minDepth(root->right))+1;
+        // return min(minDepth(root->left),minDepth(root->right))+1;
+
+         if (!root) {
+            return 0;  // Return 0 for null nodes
+        }
+
+        // If both left and right child nodes are null, return 1
+        if (!root->left && !root->right) {
+            return 1;
+        }
+
+        int leftDepth = INT_MAX;
+        int rightDepth = INT_MAX;
+
+        // Calculate the depth of the left subtree
+        if (root->left) {
+            leftDepth = minDepth(root->left);
+        }
+
+        // Calculate the depth of the right subtree
+        if (root->right) {
+            rightDepth = minDepth(root->right);
+        }
+
+        // Return the minimum depth of the left and right subtrees + 1
+        return min(leftDepth, rightDepth) + 1;
     }
 };
