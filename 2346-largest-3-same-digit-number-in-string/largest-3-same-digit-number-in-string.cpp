fb2d807bc848e57cc7c5c3e97f2c8bb14003f038
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-03-26 13:22:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (98.78%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        vector<string>v;
-        v.push_back("999");
-        v.push_back("888");
-        v.push_back("777");
-        v.push_back("666");
-        v.push_back("555");
-        v.push_back("444");
-        v.push_back("333");
-        v.push_back("222");
-        v.push_back("111");
-        v.push_back("000");
-        for(auto it :v){
-            size_t found = num.find(it);
-            if (found != string::npos)
-                return it;
+        // vector<string>v;
+        // v.push_back("999");
+        // v.push_back("888");
+        // v.push_back("777");
+        // v.push_back("666");
+        // v.push_back("555");
+        // v.push_back("444");
+        // v.push_back("333");
+        // v.push_back("222");
+        // v.push_back("111");
+        // v.push_back("000");
+        // for(auto it :v){
+        //     size_t found = num.find(it);
+        //     if (found != string::npos)
+        //         return it;
+        // }
+        // return "";
+
+        //optimal solution
+        int count=0;
+        char prev='X', maxd=' ';
+        for(char c: num){
+            if (c==prev) count++;
+            else count=1;
+            if (count==3)
+                maxd=max(maxd, c);
+            prev=move(c);
         }
-        return "";
+        return (maxd==' ')?"":string(3, maxd);
     }
 };
