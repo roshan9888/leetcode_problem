8f81f95f9a4774b1698821f94706d3752fb6bd50
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-03-31 00:20:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (20.79%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        // vector<string>ans;
+        // stack<int>s;
+        // for(int i=n;i>=1;i--){
+        //     s.push(i);
+        // }
+        // map<int,int>mp;
+        // for(int i=0;i<target.size();i++){
+        //     mp[target[i]]++;
+        // }
+        // while(!s.empty()){
+        //     int top=s.top();
+        //     s.pop();
+        //     ans.push_back("Push");
+        //     if(mp[top]){
+        //         mp.erase(top);
+        //     }else{
+        //         ans.push_back("Pop");
+        //     }
+        //     if(mp.size()==0){
+        //         return ans;
+        //     }
+        // }
+        // return ans;
+
+        vector<string> result;
+
+      int i =0;
+      int numberStream=1;
+
+
+      while(i<target.size()){
+
+          if(target[i]==numberStream){
+
+             result.push_back("Push");
+             i++;
+             numberStream++;
+
+
+          }else{
+
+
+                result.push_back("Push");
+                result.push_back("Pop");
+                numberStream++;
+
+
+          }
+
+
+
+      }
+
+      return result;
+    }
+};
