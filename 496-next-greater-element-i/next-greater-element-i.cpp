1add9f8e148f47aa22703b59b7953257003260e2
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-05-12 13:01:15 +0530

    Time: 7 ms (38.86%) | Memory: 12.1 MB (43.65%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -4,9 +4,9 @@ public:
         for(int i=0;i<nums2.size();i++){
             if(nums2[i]==a){
                 for(int j=i;j<nums2.size();j++){
-                   if(nums2[j]>a){
-                    return nums2[j];
-                   }
+                    if(nums2[j]>a){
+                        return nums2[j];
+                    }
                 }
             }
         }
@@ -17,11 +17,11 @@ public:
         unordered_map<int,int>mp;
         for(int i=0;i<nums2.size();i++){
             mp[nums2[i]]++;
-        }
+        }        
 
         for(int i=0;i<nums1.size();i++){
             if(mp[nums1[i]]){
-               ans.push_back(greater(nums1[i],nums2));
+                ans.push_back(greater(nums1[i],nums2));
             }
         }
         return ans;
