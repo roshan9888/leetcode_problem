952f47345fdc6eee207edc4a9f21148323e7c883
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-05-12 20:09:22 +0530

    Time: 7 ms (87.78%) | Memory: 23 MB (40.97%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        //brute force O(n2)
+        // int watertrapped=0;
+        // int n=height.size();
+        // for(int i=0;i<n;i++){
+        //     int j=i;
+        //     int leftmax=0,rightmax=0;
+        //     while(j>=0){
+        //         leftmax=max(leftmax,height[j]);
+        //         j--;
+        //     }
+        //     j=i;
+        //     while(j<n){
+        //         rightmax=max(rightmax,height[j]);
+        //         j++;
+        //     }
+        //     watertrapped+=min(leftmax,rightmax)-height[i];
+        // }
+        // return watertrapped;
+
+        //better solution 
+        int watertrapped=0;
         int n=height.size();
-        int left=0,right=n-1;
-        int ans=0;
-        int maxleft=0,maxright=0;
-        while(left<=right){
-            if(height[left]<=height[right]){
-                if(height[left]>=maxleft) maxleft=height[left];
-                else ans+=maxleft-height[left];
-                left++;
-            }else{
-                if(height[right]>=maxright) maxright=height[right];
-                else ans+=maxright-height[right];
-                right--;
-            }
+        vector<int>prefix(n);
+        vector<int>suffix(n);
+        prefix[0]=height[0];
+        for(int i=1;i<n;i++){
+          prefix[i]=max(prefix[i-1],height[i]);
         }
-        return ans;
+        suffix[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            suffix[i]=max(suffix[i+1],height[i]);
+        }
+
+        for(int i=0;i<n;i++){
+            watertrapped+=min(prefix[i],suffix[i])-height[i];
+        }
+        return watertrapped;
     }
 };
