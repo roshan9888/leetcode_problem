ce282c1fe1646aa10be4d1254c3fa24667370ff4
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-05-12 20:31:17 +0530

    Time: 10 ms (58.95%) | Memory: 22.3 MB (82.59%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -21,22 +21,49 @@ public:
         // return watertrapped;
 
         //better solution 
-        int watertrapped=0;
-        int n=height.size();
-        vector<int>prefix(n);
-        vector<int>suffix(n);
-        prefix[0]=height[0];
-        for(int i=1;i<n;i++){
-          prefix[i]=max(prefix[i-1],height[i]);
-        }
-        suffix[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=max(suffix[i+1],height[i]);
-        }
+        // int watertrapped=0;
+        // int n=height.size();
+        // vector<int>prefix(n);
+        // vector<int>suffix(n);
+        // prefix[0]=height[0];
+        // for(int i=1;i<n;i++){
+        //   prefix[i]=max(prefix[i-1],height[i]);
+        // }
+        // suffix[n-1]=height[n-1];
+        // for(int i=n-2;i>=0;i--){
+        //     suffix[i]=max(suffix[i+1],height[i]);
+        // }
 
-        for(int i=0;i<n;i++){
-            watertrapped+=min(prefix[i],suffix[i])-height[i];
+        // for(int i=0;i<n;i++){
+        //     watertrapped+=min(prefix[i],suffix[i])-height[i];
+        // }
+        // return watertrapped;
+
+        //optimal solution
+        int n=height.size();
+        int l=0,r=n-1;
+        int maxleft=0,maxright=0;
+        int res=0;
+        while(l<=r){
+           if(height[l]<=height[r]){
+             if(height[l]>=maxleft){
+                maxleft=height[l];
+             }
+             else{
+                res+=maxleft-height[l];
+             }
+             l++;
+           }
+           else{
+            if(height[r]>=maxright){
+                maxright=height[r];
+            }
+            else{
+                res+=maxright-height[r];
+            }
+            r--;
+           }
         }
-        return watertrapped;
+        return res;
     }
 };
