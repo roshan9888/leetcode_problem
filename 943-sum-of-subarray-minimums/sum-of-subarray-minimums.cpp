6500db9d31c4cdec559ff4abdfed16dfc2527a1a
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-05-13 10:20:10 +0530

    Time: 69 ms (40.31%) | Memory: 46.6 MB (16.94%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,43 +1,55 @@
-using ll = long long;
-const int MOD = 1e9 + 7;
-
 class Solution {
 public:
-    int sumSubarrayMins(vector<int>& nums) {
-        int length = nums.size();
-        vector<int> left(length, -1);
-        vector<int> right(length, length);
-        stack<int> stk;
+    // int find_min(vector<int>temp){
+    //     int mini=INT_MAX;
+    //     for(int i=0;i<temp.size();i++){
+    //         mini=min(mini,temp[i]);
+    //     }
+    //     return mini;
+    // }
+    int sumSubarrayMins(vector<int>& arr) {
+        //brute force 
+        // int sum=0;
+        // for(int start=0;start<arr.size();start++){
+        //     int mini=INT_MAX;
+        //     for(int end=start;end<arr.size();end++){
+        //         // vector<int>temp;
+        //         // for(int i=start;i<=end;i++){
+        //         //     temp.push_back(arr[i]);
+        //         // }
+        //         mini=min(mini,arr[end]);
+        //         sum=(sum+mini)%1000000007;
+        //     }
+        // }
+        // return sum;
 
-        for (int i = 0; i < length; ++i) {
-            while (!stk.empty() && nums[stk.top()] >= nums[i]) {
-                stk.pop();
+        //optimal solution
+        int mod=1000000007;
+        int n=arr.size();
+        vector<int>left(n,0),right(n,0);
+        stack<pair<int,int>>sleft,sright;
+        for(int i=0;i<n;i++){
+        int cnt=1;
+            while(!sleft.empty() && sleft.top().first>arr[i]){
+                cnt+=sleft.top().second;
+                sleft.pop();
             }
-            if (!stk.empty()) {
-                left[i] = stk.top();
-            }
-            stk.push(i);
+            sleft.push({arr[i],cnt});
+            left[i]=cnt;
         }
-
-        stk = stack<int>();
-
-        for (int i = length - 1; i >= 0; --i) {
-            while (!stk.empty() && nums[stk.top()] > nums[i]) {
-                stk.pop();
+        for(int i=n-1;i>=0;i--){
+        int cnt=1;
+            while(!sright.empty() && sright.top().first>=arr[i]){
+                cnt+=sright.top().second;
+                sright.pop();
             }
-            if (!stk.empty()) {
-                right[i] = stk.top();
-            }
-            stk.push(i);
+            sright.push({arr[i],cnt});
+            right[i]=cnt;
         }
-
-        ll sum = 0;
-
-        for (int i = 0; i < length; ++i) {
-            sum += static_cast<ll>(i - left[i]) * (right[i] - i) * nums[i] % MOD;
-            sum %= MOD;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=(ans+(arr[i]*(long long)(left[i]*right[i])%mod)%mod)%mod;
         }
-
-        return sum;
+        return ans;
     }
 };
