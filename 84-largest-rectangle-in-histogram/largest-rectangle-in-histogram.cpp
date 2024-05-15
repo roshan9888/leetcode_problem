06ff9a948599a30ad950c2f6a8969493047b4211
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-05-15 12:52:00 +0530

    Time: 118 ms (46.61%) | Memory: 83.9 MB (44.37%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& height) {
+        //brute force
+        // int n=height.size();
+        // int maxrec=INT_MIN;
+        // for(int i=0;i<n;i++){
+        //     int j=i;
+        //     int leftcount=0,rightcount=0;
+        //     while(j>=0){
+        //         if(height[j]<height[i]){
+        //             break;
+        //         }
+        //         else{
+        //             leftcount++;
+        //         }
+        //         j--;
+        //     }
+        //     j=i+1;
+        //     while(j<n){
+        //         if(height[j]<height[i]){
+        //             break;
+        //         }
+        //         else{
+        //         rightcount++;
+        //         }
+        //         j++;
+        //     }
+        //     maxrec=max(maxrec,(leftcount+rightcount)*height[i]);
+        // }
+        // return maxrec;
+
+        //optimal solution O(n)
+        int n=height.size();
+        vector<int>left(n);
+        vector<int>right(n);
+        stack<int>s;
+        for(int i=0;i<n;i++){
+            while(!s.empty() && height[s.top()]>=height[i]){
+                s.pop();
+            }
+            if(s.empty()) left[i]=0;
+            else left[i]=s.top()+1;
+            s.push(i);
+        }
+
+        //clear the stack for reuse
+        while(!s.empty()) s.pop();
+
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && height[s.top()]>=height[i]){
+                s.pop();
+            }
+            if(s.empty()) right[i]=n-1;
+            else right[i]=s.top()-1;
+            s.push(i);
+        }
+        int ans=INT_MIN;
+        for(int i=0;i<n;i++){
+            ans=max(ans,(right[i]-left[i]+1)*height[i]);
+        }
+        return ans;
+    }
+};
