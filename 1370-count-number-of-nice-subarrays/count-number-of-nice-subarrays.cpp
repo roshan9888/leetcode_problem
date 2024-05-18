9f05f49e86f434f89cbbabc6e734504a4a7de017
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-05-18 20:26:23 +0530

    Time: 81 ms (94.40%) | Memory: 70 MB (40.27%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -20,40 +20,66 @@ public:
         // return count;
 
         //optimal solution
-        int i=0;
-        int j=0;
-        int count=0;
-        int sum=0;
-        int xeroes=0;
-        for(int i=0;i<nums.size();i++){
+    //     int i=0;
+    //     int j=0;
+    //     int count=0;
+    //     int sum=0;
+    //     int xeroes=0;
+    //     for(int i=0;i<nums.size();i++){
+    //         if(nums[i]%2==0){
+    //             nums[i]=0;
+    //         }
+    //         else{
+    //             nums[i]=1;
+    //         }
+    //     }
+    //     for(auto it:nums){
+    //         cout<<it<<" ";
+    //     }
+    //    while(j<nums.size()){
+    //     sum+=nums[j];
+    //     while(i<j && (nums[i]==0 || sum>k)){
+    //         if(nums[i]==0){
+    //             xeroes++;
+    //         }
+    //         else{
+    //             xeroes=0;
+    //         }
+    //         sum-=nums[i];
+    //         i++;
+    //     }
+    //     if(sum==k){
+    //        count+=1+xeroes;
+    //     }
+    //     j++;
+    //    }
+    //    return count;
+
+    int i=0;
+    int j=0;
+    int count=0;
+    int odd=0;
+    int ans=0;
+    while(j<nums.size()){
+        if(nums[j]%2!=0){
+            odd++;
+            count=0;
+        }
+
+        while(odd==k){
             if(nums[i]%2==0){
-                nums[i]=0;
+                count++;
             }
-            else{
-                nums[i]=1;
+            if(nums[i]%2!=0){
+                odd--;
+                count++;
             }
-        }
-        for(auto it:nums){
-            cout<<it<<" ";
-        }
-       while(j<nums.size()){
-        sum+=nums[j];
-        while(i<j && (nums[i]==0 || sum>k)){
-            if(nums[i]==0){
-                xeroes++;
-            }
-            else{
-                xeroes=0;
-            }
-            sum-=nums[i];
             i++;
         }
-        if(sum==k){
-           count+=1+xeroes;
-        }
+        ans+=count;
         j++;
-       }
-       return count;
+    }
+    return ans;
     }
 };
 
