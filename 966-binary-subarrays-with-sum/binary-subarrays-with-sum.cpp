477de1bee5281a98b20664c4191de66fa1fcb319
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-05-18 14:12:14 +0530

    Time: 10 ms (99.23%) | Memory: 31 MB (97.80%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,19 +1,42 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
+        //brute force
+        // int count=0;
+        // for(int i=0;i<nums.size();i++){
+        //     long long sum=0;
+        //     for(int j=i;j<nums.size();j++){
+        //         sum+=nums[j];
+        //         if(sum==goal){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+
+        //optimal solution
+        int i=0;
+        int j=0;
         int count=0;
-        for(int i=0;i<nums.size();i++){
-            int s=nums[i];
-            if(s==goal){
-                count++;
+        int sum=0;
+        int xeroes=0;
+       while(j<nums.size()){
+        sum+=nums[j];
+        while(i<j && (nums[i]==0 || sum>goal)){
+            if(nums[i]==0){
+                xeroes++;
             }
-            for(int j=i+1;j<nums.size();j++){
-                s=s+nums[j];
-                if(s==goal){
-                   count++;
-                }
+            else{
+                xeroes=0;
             }
+            sum-=nums[i];
+            i++;
         }
-        return count;
+        if(sum==goal){
+           count+=1+xeroes;
+        }
+        j++;
+       }
+       return count;
     }
 };
