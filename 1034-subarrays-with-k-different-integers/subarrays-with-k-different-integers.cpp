caa7cea8cefb865dc7db0157b4abd277f0a6060f
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-05-19 23:54:40 +0530

    Time: 123 ms (23.90%) | Memory: 55.9 MB (17.01%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
-    int countSubarraysWithAtMostKDistinct(vector<int>& nums, int k){
+    int solve(vector<int>& nums, int k){
+        int l=0;
+        int r=0;
         int n=nums.size();
-        
-        map<int,int> mp;
-        
-        int i=0, j=0;
-        
-        int c=0;
-        
-        while(j<n){
-            mp[nums[j]]++;
-            
-            while(i<=j && mp.size()>k){
-                if(--mp[nums[i]] == 0) mp.erase(nums[i]);
-                i++;
+        unordered_map<int,int>mp;
+        int count=0;
+        while(r<n){
+            mp[nums[r]]++;
+            while(mp.size()>k){
+                mp[nums[l]]--;
+                if(mp[nums[l]]==0) mp.erase(nums[l]);
+                l++;
             }
-            
-            c += (j-i+1);
-            
-            j++;
+            count+=r-l+1;
+            r++;
         }
-        
-        return c;
+        return count;
     }
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return countSubarraysWithAtMostKDistinct(nums,k)-countSubarraysWithAtMostKDistinct(nums,k-1);
+        //brute force
+        // int count=0;
+        // unordered_map<int,int>mp;
+        // for(int i=0;i<nums.size();i++){
+        //     mp.clear();
+        //     for(int j=i;j<nums.size();j++){
+        //         mp[nums[j]]++;
+        //         if(mp.size()==k){
+        //             count++;
+        //         }else if(mp.size()>k){
+        //             break;
+        //         }
+        //     }
+        // }
+        // return count;
+
+        //optimal solution
+        return solve(nums,k)-solve(nums,k-1);
     }
 };
