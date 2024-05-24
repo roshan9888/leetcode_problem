2747fef16d96a30e84af8bdc74f7092d20ef59fb
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-05-25 00:33:54 +0530

    Time: 16 ms (42.79%) | Memory: 18.7 MB (98.86%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int totaljmp=0;
-        int coverage=0,lastjmp=0;
-
+        int lastjump=0;
+        int count=0;
+        int coverage=0;
         if(nums.size()==1) return 0;
-
         for(int i=0;i<nums.size();i++){
             coverage=max(coverage,i+nums[i]);
+            
+            if(i==lastjump){
+                lastjump=coverage;
+                count++;
 
-            if(i==lastjmp){
-                lastjmp=coverage;
-                totaljmp++;
-
-                if(coverage>=nums.size()-1){
-                    return totaljmp;
-                }
+                if(coverage>=nums.size()-1) return count;
             }
         }
-        return totaljmp;
+        return count;
     }
 };
