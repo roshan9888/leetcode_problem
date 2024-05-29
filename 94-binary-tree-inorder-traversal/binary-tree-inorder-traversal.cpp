3d75a9f9ac408a0a3da838a6961ee9af7ead1893
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-05-30 04:09:32 +0530

    Time: 4 ms (31.59%) | Memory: 9.7 MB (72.64%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,15 +11,45 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode*root,vector<int>&ans){
-        if(root==NULL) return;
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
+    // void inorder(TreeNode*root,vector<int>&ans){
+    //     if(root==NULL) return;
+    //     inorder(root->left,ans);
+    //     ans.push_back(root->val);
+    //     inorder(root->right,ans);
+    // }
+
+    void MorrisTraversal(TreeNode*root,vector<int>&inorder){
+        TreeNode*cur=root;
+        while(cur!=NULL){
+            if(cur->left==NULL){
+                inorder.push_back(cur->val);
+                cur=cur->right;
+            }
+            else{
+               TreeNode*prev=cur->left;
+               while(prev->right && prev->right!=cur){
+                  prev=prev->right;
+               }
+
+               if(prev->right==NULL){
+                  prev->right=cur;
+                  cur=cur->left;
+               }
+               else{
+                prev->right=NULL;
+                inorder.push_back(cur->val);
+                cur=cur->right;
+               }
+            }
+        }
     }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        inorder(root,ans);
-        return ans;
+        // vector<int>ans;
+        // inorder(root,ans);
+        // return ans;
+
+        vector<int>inorder;
+        MorrisTraversal(root,inorder);
+        return inorder;
     }
 };
