edbc4375c81da10950b64cf00397703ed2ac6c42
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-06-13 02:35:08 +0530

    Time: 77 ms (86.82%) | Memory: 95.6 MB (93.55%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    void solve(vector<int>& prices,int i,int &min_p,int &max_p){
+        if(i==prices.size()) return;
+
+        if(prices[i]<min_p){
+            min_p=prices[i];
+        }
+
+        if((prices[i]-min_p)>max_p){
+            max_p=prices[i]-min_p;
+        }
+
+        solve(prices,i+1,min_p,max_p);
+    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
 
@@ -15,22 +28,29 @@ public:
         //     }
         // }
         // return maxp;
+        
+        //iterative approach
+        // int i=0;
+        // int j=1;
+        // int sum=0;
+        // while(i<j && j<n){
+        //     int mini=prices[i];
+        //     int maxi=prices[j];
+        //     if(mini<=maxi){
+        //         sum=max(sum,maxi-mini);
+        //         j++;
+        //     }
+        //     else if(i<j && mini>maxi){
+        //         i=j;
+        //         j++;
+        //     }
+        // }
+        // return sum;
 
-        int i=0;
-        int j=1;
-        int sum=0;
-        while(i<j && j<n){
-            int mini=prices[i];
-            int maxi=prices[j];
-            if(mini<=maxi){
-                sum=max(sum,maxi-mini);
-                j++;
-            }
-            else if(i<j && mini>maxi){
-                i=j;
-                j++;
-            }
-        }
-        return sum;
+        //recursive approach
+        int max_profit=INT_MIN;
+        int min_price=INT_MAX;
+        solve(prices,0,min_price,max_profit);
+        return max_profit;
     }
 };
