fa073b2747fb2d28ae185926a5649766e4753636
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-06-13 02:55:55 +0530

    Time: 145 ms (49.97%) | Memory: 137 MB (31.98%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,42 +1,26 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        //brute force
-        // vector<int>pos;
-        // vector<int>neg;
-        // for(int i=0;i<nums.size();i++){
-        //     if(nums[i]>0){
-        //         pos.push_back(nums[i]);
-        //     }
-            //    else{
-        //         neg.push_back(nums[i]);
-        //     }
-        // }
-        // int n=nums.size()/2;
-        // for(int i=0;i<n;i++){
-        //     nums[i*2]=pos[i];
-        //     nums[i*2+1]=neg[i];
-        // }
-        // return nums;
-
-        //optimal solution
         int n=nums.size();
-        vector<int>ans(n,0);
-        int neg=1;
-        int pos=0;
+        vector<int>pos;
+        vector<int>neg;
         for(int i=0;i<nums.size();i++){
             if(nums[i]>0){
-                ans[pos]=nums[i];
-                pos+=2;
+                pos.push_back(nums[i]);
             }
             else{
-                ans[neg]=nums[i];
-                neg+=2;
+            neg.push_back(nums[i]);
             }
         }
-        return ans;
 
-        //follow up question if positive are more the negative
-        //if negatives are more then postive
+        vector<int>ans(n,0);
+        for(int i=0;i<pos.size();i++){
+            ans[2*i]=pos[i];
+        }
+
+        for(int i=0;i<neg.size();i++){
+            ans[2*i+1]=neg[i];
+        }
+        return ans;
     }
 };
