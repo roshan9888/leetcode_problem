4756030e7ff910a7354052c83db5fb353e3817c5
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-06-14 03:04:36 +0530

    Time: 12 ms (38.83%) | Memory: 16.9 MB (7.95%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        //brute force solution
-        // time complexity O(n^3)
-
-        //better solution
         int n=matrix.size();
         int m=matrix[0].size();
+        vector<vector<int>>nmatrix=matrix;
+        //brute force solution
+        // time complexity O(n^3)
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    for(int k=0;k<m;k++){
+                        nmatrix[i][k]=0;
+                    }
+
+                    for(int l=0;l<n;l++){
+                        nmatrix[l][j]=0;
+                    }
+                }
+            }
+        }
+        matrix=nmatrix;
+
+        //better solution
+       
         // vector<int>row(n,0);
         // vector<int>col(m,0);
 
@@ -30,41 +46,44 @@ public:
         // time complexity O(n^2)
 
         //optimal solution
-        int col0=1;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    if(j!=0){
-                        matrix[0][j]=0;
-                    }
-                    else{
-                        col0=0;
-                    }
-                }
-            }
-        }
+        // int col0=1;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(matrix[i][j]==0){
+        //             matrix[i][0]=0;
+        //             if(j!=0){
+        //                 matrix[0][j]=0;
+        //             }
+        //             else{
+        //                 col0=0;
+        //             }
+        //         }
+        //     }
+        // }
 
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(matrix[i][j]!=0){
-                   if(matrix[0][j]==0 || matrix[i][0]==0){
-                       matrix[i][j]=0;
-                   }
-                }
-            }
-        }
+        // for(int i=1;i<n;i++){
+        //     for(int j=1;j<m;j++){
+        //         if(matrix[i][j]!=0){
+        //            if(matrix[0][j]==0 || matrix[i][0]==0){
+        //                matrix[i][j]=0;
+        //            }
+        //         }
+        //     }
+        // }
 
-        if(matrix[0][0]==0){
-            for(int j=0;j<m;j++){
-                matrix[0][j]=0;
-            }
-        }
+        // if(matrix[0][0]==0){
+        //     for(int j=0;j<m;j++){
+        //         matrix[0][j]=0;
+        //     }
+        // }
 
-        if(col0==0){
-            for(int i=0;i<n;i++){
-                matrix[i][0]=0;
-            }
-        }
+        // if(col0==0){
+        //     for(int i=0;i<n;i++){
+        //         matrix[i][0]=0;
+        //     }
+        // }
+
+        // time complexity O(2nm)
+        // space complexity O(1)
     }
 };
