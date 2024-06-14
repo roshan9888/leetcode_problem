90962d938911c328a98f4944462f0da4fb28c1c8
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-06-15 00:53:21 +0530

    Time: 2809 ms (5.08%) | Memory: 32.8 MB (88.28%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,29 +2,29 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         //brute force solution
-        // int count=0;
-        // for(int i=0;i<nums.size();i++){   
-        //     int sum=0;  
-        //     for(int j=i;j<nums.size();j++){
-        //         sum+=nums[j];
-        //         if(sum==k){
-        //             count++;
-        //         }
-        //     }
-        // }
-        // return count;
-
-        //optimal solution
-        map<int,int>mp;
-        mp[0]=1;
-        int cnt=0,presum=0;
-        for(int i=0;i<nums.size();i++){
-            presum+=nums[i];
-            int remove=presum-k;
-            cnt+=mp[remove];
-            mp[presum]+=1;
+        int count=0;
+        for(int i=0;i<nums.size();i++){   
+            int sum=0;  
+            for(int j=i;j<nums.size();j++){
+                sum+=nums[j];
+                if(sum==k){
+                    count++;
+                }
+            }
         }
-        return cnt;
+        return count;
+       
+        //optimal solution
+        // map<int,int>mp;
+        // mp[0]=1;
+        // int cnt=0,presum=0;
+        // for(int i=0;i<nums.size();i++){
+        //     presum+=nums[i];
+        //     int remove=presum-k;
+        //     cnt+=mp[remove];
+        //     mp[presum]+=1;
+        // }
+        // return cnt;
     }
 };
 
