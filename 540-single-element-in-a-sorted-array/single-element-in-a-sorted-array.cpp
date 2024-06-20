66f2070f2e6ca0b46a31b3f9928a76b30eee9e3c
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-06-21 00:29:12 +0530

    Time: 16 ms (76.66%) | Memory: 24.6 MB (85.59%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -20,18 +20,18 @@ public:
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
             if(mid%2==0){
                if(nums[mid]!=nums[mid+1]){
-                   high=mid-1;
+                  high=mid-1;
                }
                else{
-                   low=mid+1;
+                low=mid+1;
                }
             }
             else{
                if(nums[mid]!=nums[mid-1]){
-                  high=mid-1;
+                 high=mid-1;
                }
                else{
-                   low=mid+1;
+                low=mid+1;
                }
             }
         }
