e58dffd9fae5cf7829b0eb1a933ece35000b4bdf
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-06-22 01:54:11 +0530

    Time: 112 ms (72.13%) | Memory: 69.1 MB (26.72%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,37 +1,68 @@
 class Solution {
 public:
-    int func(vector<int>& bloomDay,int mid,int k,int m){
-        int count=0;
-        int nofb=0;
-        for(int i=0;i<bloomDay.size();i++){
-            if(bloomDay[i]<=mid){
-                count++;
-            }
-            else{
-                nofb+=(count/k);
-                count=0;
-            }
-        }
-        nofb+=(count/k);
-        cout<<count<<" ";
-        return nofb>=m;
+    bool possible(vector<int>&bloomDay,int day,int k,int m){
+        int bloomno=0;
+        int cnt=0;
+       for(int i=0;i<bloomDay.size();i++){
+          if(bloomDay[i]<=day){
+            cnt++;
+          }
+          else{
+            bloomno+=cnt/k;
+            cnt=0;
+          }
+       }
+        bloomno+=cnt/k;
+       return bloomno>=m;
     }
 
+    // int func(vector<int>&bloomDay,int mid,int m,int k){
+    //     long long bloom=0;
+    //     for(int i=1;i<=bloomDay.size();i++){
+    //         if(bloomDay[i]<=mid){
+    //             bloom++;
+    //         }else{
+    //             bloomno=bloom/k;
+    //             bloom=0;
+    //         }
+    //     }
+    //     bloomno=bloom/k;
+
+    //     if(bloomno>=m) return 2;
+    //     return 1;
+    // }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int n=bloomDay.size();
-        int mini=INT_MAX;
+        //brute force
+        // long long  val=1ll*m*1ll*k;
+        // if(val>bloomDay.size()) return -1;
+        // int mini=INT_MAX;
+        // int maxi=INT_MIN;
+        // for(int i=0;i<bloomDay.size();i++){
+        //     maxi=max(maxi,bloomDay[i]);
+        //     mini=min(mini,bloomDay[i]);
+        // }
+        // for(int i=mini;i<=maxi;i++){
+        //     if(possible(bloomDay,i,k,m)){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        //optimal solution
+        long long  val=1ll*m*1ll*k;
+        if(val>bloomDay.size()) return -1;
         int maxi=INT_MIN;
-        long long target=m*1LL*k*1LL;
-        if(target>n) return -1;
-        for(int i=0;i<n;i++){
-            mini=min(mini,bloomDay[i]);
+        int mini=INT_MAX;
+        for(int i=0;i<bloomDay.size();i++){
             maxi=max(maxi,bloomDay[i]);
+            mini=min(mini,bloomDay[i]);
         }
         int low=mini;
         int high=maxi;
         while(low<=high){
-            int mid=low+(high-low)/2;
-            if(func(bloomDay,mid,k,m)){
+            int mid=(low+high)/2;
+            // int m=func(bloomDay,mid,m,k);
+            if(possible(bloomDay,mid,k,m)){
                 high=mid-1;
             }
             else{
