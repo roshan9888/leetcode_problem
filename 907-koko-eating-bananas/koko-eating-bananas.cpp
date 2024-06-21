490ce2585e4815f3c27f138043014a9f3c0af737
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-06-22 00:36:38 +0530

    Time: 35 ms (42.40%) | Memory: 21.4 MB (64.97%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    long long func(vector<int>& piles,int k){
+    long long func(vector<int>&piles,int k){
         long long totaltime=0;
         for(int i=0;i<piles.size();i++){
             totaltime+=ceil((double)piles[i]/(double)(k));
         }
-        
         return totaltime;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
@@ -13,22 +12,18 @@ public:
         for(int i=0;i<piles.size();i++){
             maxi=max(maxi,piles[i]);
         }
-        // cout<<maxi<<" ";
         int low=1;
         int high=maxi;
         while(low<=high){
-            // int mid=low+(high-low)/2;
-            int mid=(low+high)/2;
-
-            long long  midN=func(piles,mid);
-            if(midN<=h){
-                 high=mid-1;
+            long long mid=(low+high)/2;
+            long long m=func(piles,mid);
+            if(m<=h){
+                high=mid-1;
             }
             else{
-                low=mid+1;  
+                low=mid+1;
             }
         }
         return low;
     }
 };
-
