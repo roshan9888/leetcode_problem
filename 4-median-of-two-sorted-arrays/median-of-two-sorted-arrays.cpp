94b378d9b33d1c86e550fce66ef2147baa46ff3a
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-07-11 11:04:17 +0530

    Time: 33 ms (20.81%) | Memory: 94.5 MB (72.78%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m=nums1.size();
-        int n=nums2.size();
-        int i=m-1;
-        int j=n-1;
-        int k=m+n-1;
-        // int n1=nums1.size();
-        // int n2=nums2.size();
-        nums1.resize(m+n);
-        while(j>=0){
-            if(i>=0 && nums1[i]>nums2[j]){
-                nums1[k--]=nums1[i--];
-            }
-            else{
-                nums1[k--]=nums2[j--];
-            }
-        }
-        for(auto it:nums1){
-            cout<<it<<" ";
-        }
-        int low=0;
-        int high=nums1.size()-1;
-        if(nums1.size()%2==0){
-            int mid1=(low+high)/2;
-            int mid2=mid1+1;
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
+        int n1 = a.size(), n2 = b.size();
+    //if n1 is bigger swap the arrays:
+    if (n1 > n2) return findMedianSortedArrays(b, a);
 
-            return double(nums1[mid1]+nums1[mid2])/2.0;
+    int n = n1 + n2; //total length
+    int left = (n1 + n2 + 1) / 2; //length of left half
+    //apply binary search:
+    int low = 0, high = n1;
+    while (low <= high) {
+        int mid1 = (low + high) >> 1;
+        int mid2 = left - mid1;
+        //calculate l1, l2, r1 and r2;
+        int l1 = INT_MIN, l2 = INT_MIN;
+        int r1 = INT_MAX, r2 = INT_MAX;
+        if (mid1 < n1) r1 = a[mid1];
+        if (mid2 < n2) r2 = b[mid2];
+        if (mid1 - 1 >= 0) l1 = a[mid1 - 1];
+        if (mid2 - 1 >= 0) l2 = b[mid2 - 1];
+
+        if (l1 <= r2 && l2 <= r1) {
+            if (n % 2 == 1) return max(l1, l2);
+            else return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
         }
-        else{
-             int mid1=(low+high)/2;
-             return double(nums1[mid1]);
-        }
-        return 0;
+
+        //eliminate the halves:
+        else if (l1 > r2) high = mid1 - 1;
+        else low = mid1 + 1;
+    }
+    return 0; //dummy statement
     }
 };
