caa56f3e57283a785c779545acb3fc86641e1ddc
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-07-19 23:17:36 +0530

    Time: 193 ms (16.08%) | Memory: 126.1 MB (21.25%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,54 +10,25 @@
  */
 class Solution {
 public:
-ListNode* reverseLinkedlList(ListNode*head){
-    if(head==NULL || head->next==NULL) return head;
-    ListNode* newHead=reverseLinkedlList(head->next);
-    ListNode*front=head->next;
-    front->next=head;
-    head->next=NULL;
-    return newHead;
-}
     bool isPalindrome(ListNode* head) {
         //brute force
-        // ListNode* temp=head;
-        // stack<int>st;
-        // while(temp!=NULL){
-        //     st.push(temp->val);
-        //     temp=temp->next;
-        // }
-
-        // temp=head;
-        // while(temp!=NULL){
-        //     if(temp->val!=st.top()){
-        //         return false;   
-        //     }
-        //     temp=temp->next;
-        //     st.pop();
-        // }
-        // if(st.size()==0) return true;
-        // return false;
-
-        //optimal solution
-        if(head==NULL || head->next==NULL) return true;
-        ListNode*slow=head;
-        ListNode*fast=head;
-        while(fast->next!=NULL && fast->next->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
+        ListNode*temp=head;
+        stack<int>st;
+        while(temp!=NULL){
+            st.push(temp->val);
+            temp=temp->next;
         }
-        ListNode*newHead=reverseLinkedlList(slow->next);
-        ListNode*first=head;
-        ListNode*second=newHead;
-        while(second!=NULL){
-            if(first->val !=second->val){
-                reverseLinkedlList(newHead);
+
+        temp=head;
+        while(temp!=NULL){
+            if(temp->val!=st.top()){
                 return false;
             }
-            first=first->next;
-            second=second->next;
+            else{
+                st.pop();
+                temp=temp->next;
+            }
         }
-        reverseLinkedlList(newHead);
         return true;
     }
 };
