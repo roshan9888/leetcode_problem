ac06ecd1d3fcfae6440bebd3d1be4b63feaae19f
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-07-28 22:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (9.86%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,40 +1,20 @@
-#define ll long long
 class Solution {
 public:
-    int p = 1e9 + 7;
-    ll power(long long x, long long y) {
-      long long res = 1;    
+    long long Power(long long N,long long R){
+        if(R==0) return 1;
+        if(R==1) return (N%1000000007);
 
-      x = x % p; // Update x if it is more than or equal to p
-      if (x == 0) return 0; 
-
-      while (y > 0) {
-        // If y is odd, multiply x with result
-        if (y & 1) res = (res*x) % p;
-        
-        // we have did the odd step is it was odd, now do the even step
-        y = y>>1; // dividing y by 2, y>>1 is same as y/2
-        x = (x*x) % p;
-      }
-      return res;
+        long long ans=1;
+        long long know=(Power(N,R/2)%1000000007);
+        if(R%2){
+            ans=(ans*N)%1000000007;
+        }
+        return (ans*((know*know)%1000000007))%1000000007;
     }
     int countGoodNumbers(long long n) {
-        // int even=5;
-        // int odd=4;
-        // long long int ans=1;
-        // for(int i=0;i<n;i++){
-        //     if(i%2==0){
-        //         ans=(ans*even)%1000000007;
-        //     }
-        //     else{
-        //         ans=(ans*odd)%1000000007;
-        //     }
-        // }
-        // return static_cast<int>(ans);
-
-      ll count_of_4s = n/2;
-      ll count_of_5s = n - count_of_4s;
-      ll ans = ((power(4LL, count_of_4s) % p  * power(5LL, count_of_5s) % p) % p);
-      return (int)ans;
+        if(n%2){
+            return (5*Power(20,n/2))%1000000007;
+        }
+        return Power(20,n/2);
     }
 };
