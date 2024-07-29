460e0c242961c4fea6978e4479574d75b6ad576b
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-07-29 21:30:19 +0530

    Time: 97 ms (50.67%) | Memory: 61.3 MB (40.70%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    bool checkPal(string &str, int start, int end){
-        while(start <= end){
-            if(str[start] != str[end]){
-                return false;
-            }
-            start++;
-            end--;
+    bool isPalindrome(string s,int start,int end){
+        while(start<=end){
+        if(s[start]!=s[end]){
+            return false;
+        }
+        start++;
+        end--;
         }
         return true;
     }
-    void generatePalindrome(int idx, vector<string> &temp, vector<vector<string>> &result, string &s){
-        if(idx == s.size()){
-            result.push_back(temp);
+    void solve(string s,int ind,vector<vector<string>>&ans,vector<string>&temp){
+        if(ind>=s.size()){
+            ans.push_back(temp);
             return;
         }
-        
-        for(int i = idx; i < s.size(); i++){
-            if(checkPal(s, idx, i)){
-                temp.push_back(s.substr(idx, i - idx + 1));
-                generatePalindrome(i + 1, temp, result, s);
-                temp.pop_back();
+
+        for(int i=ind;i<s.size();i++){
+            if(isPalindrome(s,ind,i)){
+               temp.push_back(s.substr(ind,i-ind+1));
+               solve(s,i+1,ans,temp);
+               temp.pop_back();
             }
+            
         }
     }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> result;
-        vector<string> temp;
-        generatePalindrome(0, temp, result, s);
-        return result;
+        vector<vector<string>>ans;
+        vector<string>temp;
+        solve(s,0,ans,temp);
+        return ans; 
     }
 };
