96a1f062231cd1486484529a6e44b6ae25713eb4
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-07-31 17:55:19 +0530

    Time: 3 ms (29.59%) | Memory: 7.7 MB (14.78%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -21,12 +21,26 @@ public:
         return dp[n]=topdown(n-1,dp)+topdown(n-2,dp);
     }
 
+    void bottomup(int n,vector<int>&dp){
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+    }
+
     int climbStairs(int n) {
         // int count=0;
         // return solve(n,count);
 
         //top down dp
+        // vector<int>dp(n+1,-1);
+        // return topdown(n,dp);
+
+        //botto up dp
         vector<int>dp(n+1,-1);
-        return topdown(n,dp);
+        bottomup(n,dp);
+        return dp[n];
     }
 };
