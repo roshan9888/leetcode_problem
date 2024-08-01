0320d13b63322183ad67a7a379d6485680ad9430
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-02 00:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (15.30%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -18,12 +18,31 @@ public:
         return dp[i][j]=min(down,dia);
     }
 
+    int bottomup(int n,vector<vector<int>>&triangle,vector<vector<int>>&dp){
+        for(int j=0;j<n;j++){
+            dp[n-1][j]=triangle[n-1][j];
+        }
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+               int down=triangle[i][j]+dp[i+1][j];
+               int dia=triangle[i][j]+dp[i+1][j+1];
+               dp[i][j]=min(down,dia);
+            }
+        }
+        return dp[0][0];
+    }
+
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         // return solve(0,0,triangle);
 
         //top down dp
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return topdown(0,0,triangle,dp);
+        // vector<vector<int>>dp(n,vector<int>(n,-1));
+        // return topdown(0,0,triangle,dp);
+
+        //bottom up dp
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        return bottomup(n,triangle,dp);
     }
 };
