b096be446b7183a3c254ad220946800ae4989b09
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-01 18:12:10 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (18.23%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -8,8 +8,8 @@ public:
         if(i<0 || j<0) return 0;
 
         int left=solve(i,j-1);
-        int right=solve(i-1,j);
-        return left+right;
+        int up=solve(i-1,j);
+        return left+up;
     }
 
     int topdown(int i,int j,vector<vector<int>>&dp){
@@ -21,14 +21,33 @@ public:
 
         if(dp[i][j]!=-1) return dp[i][j]; 
         int left=topdown(i,j-1,dp);
-        int right=topdown(i-1,j,dp);
-        return dp[i][j]=left+right;
+        int up=topdown(i-1,j,dp);
+        return dp[i][j]=left+up;
+    }
+
+    int bottomup(int m,int n,vector<vector<int>>&dp){
+        dp[0][0]=1;
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                else{
+                    int left=j>0 ? dp[i][j-1] : 0;
+                    int up=i > 0 ? dp[i-1][j] : 0;
+                    dp[i][j]=left+up;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 
     int uniquePaths(int m, int n) {
         // return solve(m-1,n-1);
         
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return topdown(m-1,n-1,dp);
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // return topdown(m-1,n-1,dp);
+
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        return bottomup(m,n,dp);
     }
 };
