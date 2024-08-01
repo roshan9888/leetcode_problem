b0c0c3114ba1659472738288adc577959b7010c5
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-01 18:33:28 +0530

    Time: 3 ms (34.05%) | Memory: 7.6 MB (55.53%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -40,14 +40,33 @@ public:
         }
         return dp[m-1][n-1];
     }
-
+    
+    int space(int m,int n){
+        vector<int>prev(n,0);
+        for(int i=0;i<m;i++){
+            vector<int>curr(n,0);
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) curr[j]=1;
+                else{
+                    int up=i > 0 ? prev[j] : 0;
+                    int left=j>0 ? curr[j-1] : 0;
+                    curr[j]=left+up;
+                }
+            }
+            prev=curr;
+        }
+        return prev[n-1];
+    }
     int uniquePaths(int m, int n) {
         // return solve(m-1,n-1);
         
         // vector<vector<int>>dp(m,vector<int>(n,-1));
         // return topdown(m-1,n-1,dp);
 
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        return bottomup(m,n,dp);
+        // vector<vector<int>>dp(m,vector<int>(n,0));
+        // return bottomup(m,n,dp);
+
+        //space 
+        return space(m,n);
     }
 };
