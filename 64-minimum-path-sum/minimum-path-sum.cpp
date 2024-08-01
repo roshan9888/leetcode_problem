c63df04da523c1bf6307f30182a2410d1d8efd6c
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-01 23:11:42 +0530

    Time: 12 ms (7.51%) | Memory: 12.9 MB (25.05%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -34,6 +34,22 @@ public:
         mini=min(left,up);
         return dp[i][j]=mini;
     }
+    
+    int bottomup(int m,int n,vector<vector<int>>&grid,vector<vector<int>>&dp){
+        int mini=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[0][0]=grid[0][0];
+                else{
+                  int left=j>0?grid[i][j]+dp[i][j-1] : INT_MAX;
+                  int up=i>0?grid[i][j]+dp[i-1][j]:INT_MAX;
+                  dp[i][j]=min(left,up);
+                }
+                
+            }
+        }
+        return dp[m-1][n-1];
+    }
 
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
@@ -41,7 +57,11 @@ public:
         // return solve(m-1,n-1,grid);
 
         //top down dp
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return topdown(m-1,n-1,grid,dp);
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // return topdown(m-1,n-1,grid,dp);
+
+        //bottomup dp
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        return bottomup(m,n,grid,dp);
     }
 };
