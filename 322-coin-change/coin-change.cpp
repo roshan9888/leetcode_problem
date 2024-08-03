af42ad83e10500552c9337aa721b21b0633d2a75
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-03 16:28:38 +0530

    Time: 52 ms (23.94%) | Memory: 47.1 MB (37.32%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,33 +1,73 @@
 class Solution {
-public: 
-    int solve(vector<int>&coins,int amount,vector<int>&dp){
-        if(amount==0){
-            return 0;
+public:
+    int solve(vector<int>&coins,int target,int i){
+        if(i==0){
+        if(target%coins[i]==0){
+            return target/coins[i];
+        }else{
+            return 1e8;
         }
-        if(amount<0){
-            return INT_MAX;
         }
-        if(dp[amount]!=-1){
-            return dp[amount];
+        
+        int notpick=solve(coins,target,i-1);
+        int pick=1e8;
+        if(coins[i]<=target){
+            pick=1+solve(coins,target-coins[i],i);
         }
-        int mini=INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            int ans=solve(coins,amount-coins[i],dp);
-            if(ans!=INT_MAX){
-                mini=min(mini,ans+1);
+        return min(pick,notpick);
+    }
+    
+    int topdown(vector<int>&coins,int target,int i,vector<vector<int>>&dp){
+        if(i==0){
+        if(target%coins[i]==0){
+            return target/coins[i];
+        }else{
+            return 1e8;
+        }
+        }
+        if(dp[i][target]!=-1) return dp[i][target];
+        int notpick=topdown(coins,target,i-1,dp);
+        int pick=1e8;
+        if(coins[i]<=target){
+            pick=1+topdown(coins,target-coins[i],i,dp);
+        }
+        return dp[i][target]=min(pick,notpick);
+    }
+    
+    int bottomup(vector<int>&coins,int target,int n,vector<vector<int>>&dp){
+        for(int t=0;t<=target;t++){
+            if(t%coins[0]==0){
+                dp[0][t]=t/coins[0];
+            }
+            else{
+                dp[0][t]=1e8;
             }
         }
-        dp[amount]=mini;
-        return dp[amount];
+        
+        for(int i=1;i<n;i++){
+            for(int t=0;t<=target;t++){
+                    int notpick=dp[i-1][t];
+                    int pick=1e8;
+                    if(coins[i]<=t){
+                        pick=1+dp[i][t-coins[i]];
+                    }
+                    dp[i][t]=min(pick,notpick);
+            }
+        }
+        return dp[n-1][target];
     }
+
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,-1);
-        int ans=solve(coins,amount,dp);
-        if(ans!=INT_MAX){
-            return ans;
-        }
-        else{
-            return -1;
-        }
+        int n=coins.size();
+        // int ans=solve(coins,amount,n-1);
+        // return ans==1e8?-1:ans;
+
+        // vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        // int ans=topdown(coins,amount,n-1,dp);
+        // return ans==1e8?-1:ans;
+
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        int ans=bottomup(coins,amount,n,dp);
+        return (ans>=1e8)?-1:ans;
     }
 };
