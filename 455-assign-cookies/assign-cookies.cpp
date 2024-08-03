e97755021e58e2ce3a6822e093dc8a937f937f24
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-03 06:14:26 +0530

    Time: 17 ms (82.63%) | Memory: 20.9 MB (33.07%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        map<int,int>mp;
-        for(int i=0;i<s.size();i++){
-            mp[s[i]]++;
+        int n=g.size();
+        int m=s.size();
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int l=0,r=0;
+        while(l<m && r<n){
+           if(s[l]>=g[r]){
+              r=r+1;
+           }
+           l=l+1;
         }
-        int child=0;
-        for(int i=0;i<g.size();i++){
-            if(mp.find(g[i])!=mp.end()){
-                mp[g[i]]--;
-                if(mp[g[i]]==0) mp.erase(g[i]);
-               child++;
-            }else{
-                auto it = mp.upper_bound(g[i]); // Find the first element greater than g[i]
-            if (it != mp.end()) {
-                it->second--;
-                if(it->second==0){
-                    mp.erase(it);
-                }
-                child++;
-            }
-            }
-        }
-        return child;
+        return r;
     }
 };
