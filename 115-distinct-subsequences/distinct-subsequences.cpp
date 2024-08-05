7d10d9eb7e54d738a7251cd48b5f2ede3aad0d6f
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-06 05:29:55 +0530

    Time: 18 ms (61.98%) | Memory: 31.8 MB (24.31%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -30,13 +30,33 @@ public:
         }
     }
 
+    int bottomup(int n,int m,string&s,string&t,vector<vector<double>>&dp){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                   dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return (int)dp[n][m];
+    }
+
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
         // return solve(n-1,m-1,s,t);
         
         //top down dp 
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return topdown(n-1,m-1,s,t,dp);
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
+        // return topdown(n-1,m-1,s,t,dp);
+
+        //bottom up dp
+        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+        for(int j=1;j<=m;j++) dp[0][j]=0;
+        return bottomup(n,m,s,t,dp);
     }
 };
