264dfcdcc9eddbe99f07347264deac0e7c4d551d
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-07 01:15:29 +0530

    Time: 5 ms (33.29%) | Memory: 14.8 MB (19.66%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public: 
+    int solve(int ind,vector<int>&prices,bool buy){
+        if(ind>=prices.size()) return 0;
+        long profit=0;
+        if(buy){
+           profit=max((-prices[ind]+solve(ind+1,prices,0)),(solve(ind+1,prices,1)));
+        }else{
+           profit=max((prices[ind]+solve(ind+2,prices,1)),(solve(ind+1,prices,0)));
+        }
+        return profit;
+    }
+
+    int topdown(int ind,vector<int>&prices,bool buy,vector<vector<int>>&dp){
+        if(ind>=prices.size()) return 0;
+        long profit=0;
+        if(dp[ind][buy]!=-1) return dp[ind][buy];
+        if(buy){
+           profit=max((-prices[ind]+topdown(ind+1,prices,0,dp)),(topdown(ind+1,prices,1,dp)));
+        }else{
+           profit=max((prices[ind]+topdown(ind+2,prices,1,dp)),(topdown(ind+1,prices,0,dp)));
+        }
+        return dp[ind][buy]=profit;
+    }
+
+    int bottomup(int n,vector<int>&prices,int buy,vector<vector<int>>&dp){
+        dp[n][0]=0;
+        dp[n][1]=0;
+        // if(ind==prices.size()) return 0;
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                long profit=0;
+                if(buy){
+                    profit=max((-prices[ind]+dp[ind+1][0]),(dp[ind+1][1]));
+                }else{
+                    profit=max((prices[ind]+dp[ind+1][1]),(dp[ind+1][0]));
+                }
+                dp[ind][buy]=profit;
+            }
+        }
+        return dp[0][1];
+    }
+
+    int space(int n,vector<int>&prices,int buy){
+        vector<int>prev(n+1,0),curr(n+1,0);
+        prev[0]=0;
+        prev[1]=0;
+        // if(ind==prices.size()) return 0;
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                long profit=0;
+                if(buy){
+                    profit=max((-prices[ind]+prev[0]),(prev[1]));
+                }else{
+                    profit=max((prices[ind]+prev[1]),(prev[0]));
+                }
+                curr[buy]=profit;
+            }
+            prev=curr;
+        }
+        return curr[1];
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        // return solve(0,prices,1);
+
+        //top down dp
+    vector<vector<int>>dp(n,vector<int>(2,-1));
+    return topdown(0,prices,1,dp);
+    
+    //bottom up dp
+    // vector<vector<int>>dp(n+1,vector<int>(2,0));
+    // return bottomup(n,prices,1,dp);
+
+    //space
+    // return space(n,prices,1);
+    }
+};
