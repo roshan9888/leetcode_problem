2a46bdd21dbd47f4038d6436061b516506dbe087
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-06 23:11:17 +0530

    Time: 12 ms (15.13%) | Memory: 18.2 MB (22.76%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -23,12 +23,35 @@ public:
         return dp[ind][buy]=profit;
     }
 
+    int bottomup(int n,vector<int>&prices,int buy,vector<vector<int>>&dp){
+        dp[n][0]=0;
+        dp[n][1]=0;
+        // if(ind==prices.size()) return 0;
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                long profit=0;
+                if(buy){
+                    profit=max((-prices[ind]+dp[ind+1][0]),(dp[ind+1][1]));
+                }else{
+                    profit=max((prices[ind]+dp[ind+1][1]),(dp[ind+1][0]));
+                }
+                dp[ind][buy]=profit;
+            }
+        }
+        return dp[0][1];
+    }
+
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
     //    return solve(0,prices,1);
 
     //top down dp
-    vector<vector<int>>dp(n,vector<int>(2,-1));
-    return topdown(0,prices,1,dp);
+    // vector<vector<int>>dp(n,vector<int>(2,-1));
+    // return topdown(0,prices,1,dp);
+    
+    //bottom up dp
+    vector<vector<int>>dp(n+1,vector<int>(2,0));
+    return bottomup(n,prices,1,dp);
     }
 };
