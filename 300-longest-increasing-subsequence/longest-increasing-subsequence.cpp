1b81695213d2e7e60d5a7784da96d19d876a60b0
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-07 03:58:18 +0530

    Time: 236 ms (36.86%) | Memory: 294.8 MB (17.07%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -20,12 +20,30 @@ public:
         return dp[ind][pre_ind+1]=len;
     }
 
+    int bottomup(int n,vector<int>&nums,int pre_ind,vector<vector<int>>&dp){
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int pre_ind=ind-1;pre_ind>=-1;pre_ind--){
+                int len=dp[ind+1][pre_ind+1];
+                if(pre_ind==-1 || nums[ind]>nums[pre_ind]){
+                    len=max(len,1+dp[ind+1][ind+1]);
+                }
+                dp[ind][pre_ind+1]=len;
+            } 
+        }
+        return dp[0][0];
+    }
+
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         // return solve(0,nums,-1);
 
         //top down dp
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return topdown(0,nums,-1,dp);
+        // vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        // return topdown(0,nums,-1,dp);
+
+        //bottomup dp
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        return bottomup(n,nums,-1,dp);
     }
 };
