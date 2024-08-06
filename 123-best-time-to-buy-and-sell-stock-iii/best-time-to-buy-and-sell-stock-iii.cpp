f3ba120649f64c8ce90906aa48ef3bfe7722c47b
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-07 00:07:55 +0530

    Time: 480 ms (24.44%) | Memory: 222.6 MB (14.41%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int solve(int ind,vector<int>&prices,bool buy,int count){
+        if(count==0) return 0;
+        if(ind==prices.size()) return 0;
+        long profit=0;
+        if(buy){
+           profit=max((-prices[ind]+solve(ind+1,prices,0,count)),(solve(ind+1,prices,1,count)));
+        }else{
+           profit=max((prices[ind]+solve(ind+1,prices,1,count-1)),(solve(ind+1,prices,0,count)));
+        }
+        return profit;
+    }
+
+    int topdown(int ind,vector<int>&prices,int buy,int count,vector<vector<vector<int>>>&dp){
+        if(ind==prices.size() || count==0) return 0;
+        // if() return 0;
+        if(dp[ind][buy][count]!=-1) return dp[ind][buy][count];
+        long profit=0;
+        if(buy){
+           profit=max((-prices[ind]+topdown(ind+1,prices,0,count,dp)),(topdown(ind+1,prices,1,count,dp)));
+        }else{
+           profit=max((prices[ind]+topdown(ind+1,prices,1,count-1,dp)),(topdown(ind+1,prices,0,count,dp)));
+        }
+        return dp[ind][buy][count]=profit;
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        // return solve(0,prices,1,2);
+
+        //topdown dp
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
+        return topdown(0,prices,1,2,dp);
+    }
+};
