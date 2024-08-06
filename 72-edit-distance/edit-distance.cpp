a8917b4e961519d942885eb5e696dc1dea78e579
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-06 17:56:42 +0530

    Time: 6 ms (71.10%) | Memory: 13 MB (28.63%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -30,12 +30,12 @@ public:
     }
 
     int bottomup(int n,int m,string &word1,string &word2,vector<vector<int>>&dp){
-        for(int i=0;i<=n;i++) dp[0][i]=i;
-        for(int j=0;j<=m;j++) dp[j][0]=j;
+        for(int i=0;i<=m;i++) dp[0][i]=i;
+        for(int j=0;j<=n;j++) dp[j][0]=j;
          
-        for(int i=1;i<-n;i++){
-            for(int j=0;j<=m;j++){
-                if(word1[i]==word2[j]){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]){
                 dp[i][j]=dp[i-1][j-1];
         }
         else{
@@ -50,11 +50,13 @@ public:
         int n=word1.size();
         int m=word2.size();
         // return solve(n,m,word1,word2);
-
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return topdown(n-1,m-1,word1,word2,dp);
-
+        
+        //top down dp
         // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
         // return topdown(n-1,m-1,word1,word2,dp);
+        
+        //bottom up dp
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        return bottomup(n,m,word1,word2,dp);
     }
 };
