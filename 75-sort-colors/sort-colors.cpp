cc3c1aa3acc258671e48576b8ded088ece728930
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-10 00:34:47 +0530

    Time: 5 ms (15.72%) | Memory: 10.3 MB (7.91%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -11,45 +11,45 @@ public:
         // }
 
         //better solution
-        int zeroes=0;
-        int ones=0;
-        int twos=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==1){
-                ones++;
-            }
-            else if(nums[i]==2){
-                twos++;
-            }
-            else{
-                zeroes++;
-            }   
-        }
-        for(int i=0;i<zeroes;i++){
-            nums[i]=0;
-        }
-        for(int i=zeroes;i<zeroes+ones;i++){
-            nums[i]=1;
-        }
-        for(int i=zeroes+ones;i<nums.size();i++){
-            nums[i]=2;
-        }
-
-        //optimal solution
-        // int s=0,mid=0,e=nums.size()-1;
-        // while(s<=e){
-        //     if(nums[mid]==0){
-        //         swap(nums[mid],nums[s]);
-        //         s++;
-        //         mid++;
+        // int zeroes=0;
+        // int ones=0;
+        // int twos=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==1){
+        //         ones++;
         //     }
-        //     else if(nums[mid]==1){
-        //         mid++;
+        //     else if(nums[i]==2){
+        //         twos++;
         //     }
         //     else{
-        //         swap(nums[mid],nums[e]);
-        //         e--;
-        //     }
+        //         zeroes++;
+        //     }   
         // }
+        // for(int i=0;i<zeroes;i++){
+        //     nums[i]=0;
+        // }
+        // for(int i=zeroes;i<zeroes+ones;i++){
+        //     nums[i]=1;
+        // }
+        // for(int i=zeroes+ones;i<nums.size();i++){
+        //     nums[i]=2;
+        // }
+
+        //optimal solution
+        int s=0,mid=0,e=nums.size()-1;
+        while(mid<=e){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[s]);
+                s++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[e]);
+                e--;
+            }
+        }
     }
 };
