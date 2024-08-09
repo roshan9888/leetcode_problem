2e2de32fc2e44616b4740e86a9df8b6418fdde9a
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-09 17:04:05 +0530

    Time: 22 ms (18.52%) | Memory: 21.9 MB (19.24%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        //brute force
-        vector<int>temp(nums.size(),0);
-        int j=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0){
-                temp[j]=nums[i];
-                j++;
-            }
-        }
-        for(int i=0;i<temp.size();i++){
-            nums[i]=temp[i];
-        }
-        
-        // int left=0;
-        // int n=nums.size();
-        // for(int i=0;i<n;i++){
+        //brute force O(2n) space O(n)
+        // vector<int>temp(nums.size(),0);
+        // int j=0;
+        // for(int i=0;i<nums.size();i++){
         //     if(nums[i]!=0){
-        //         nums[left]=nums[i];
-        //         left++;
+        //         temp[j]=nums[i];
+        //         j++;
         //     }
         // }
-        // while(left<n){
-        //     nums[left]=0;
-        //     left++;
+
+        // for(int i=0;i<temp.size();i++){
+        //     nums[i]=temp[i];
         // }
+
+        //better
+        int left=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[left]=nums[i];
+                left++;
+            }
+           
+        }
+        while(left<n){
+            nums[left]=0;
+            left++;
+        }
     }
 };
