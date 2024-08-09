8a03fba02979a1cc53de4679d641845db6f47bf9
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-10 05:20:29 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (5.58%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -2,36 +2,13 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        int m=matrix[0].size();
-        // ..brute force 
-        // vector<vector<int>>nm(n,vector<int>(m));
-        // for(int i=0;i<n;i++){
-        //     int k=m;
-        //     // cout<<k<<" ";
-        //     for(int j=0;j<m;j++){
-        //         k--;
-        //         nm[i][j]=matrix[k][i];
-        //         cout<<nm[i][j]<<" ";
-        //         // cout<<k<<" ";
-        //     }
-        // }
-        //n-1-i formula
-
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         matrix[i][j]=nm[i][j];
-        //     }
-        // } 
-
-        //optimal solution
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                swap(matrix[i][j],matrix[j][i]);
+        int m=matrix.size();
+        vector<vector<int>>nm(n,vector<int>(m));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                nm[j][n-1-i]=matrix[i][j];
             }
         }
-        
-        for(int i=0;i<n;i++){
-            reverse(matrix[i].begin(),matrix[i].end());
-        }
+        matrix=nm;
     }
 };
