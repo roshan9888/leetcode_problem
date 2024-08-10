72c0907f9d15e55fe4d1400939d244b8d991a18d
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-11 02:35:47 +0530

    Time: 63 ms (63.05%) | Memory: 29.3 MB (41.27%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         // brute force 
-        // vector<vector<int>>ans;
-        // sort(nums.begin(),nums.end());
-        // set<vector<int>>set;
-        // for(int i=0;i<nums.size()-2;i++){
-        //     for(int j=i+1;j<nums.size()-1;j++){
-        //         for(int k=j+1;k<nums.size();k++){
-        //             if(nums[i]+nums[j]+nums[k]==0 && i!=j && i!=k && j!=k){
-        //                set.insert({nums[i],nums[j],nums[k]});
+        // int n=nums.size();
+        // set<vector<int>>st;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         for(int k=j+1;k<n;k++){
+        //             if(nums[i]+nums[j]+nums[k]==0){
+        //                vector<int>temp={nums[i],nums[j],nums[k]};
+        //                sort(temp.begin(),temp.end());
+        //                st.insert(temp);
         //             }
         //         }
         //     }
-        // f}
-        // for(auto it:set){
-        //     ans.push_back(it);
         // }
+        // vector<vector<int>>ans(st.begin(),st.end());
         // return ans;
 
         // time complexity O(n^3)
@@ -40,28 +39,29 @@ public:
         // return ans;
 
         //optimal solution
+        int n=nums.size();
         vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
-            int j=i+1;
-            int k=nums.size()-1;
 
+            int j=i+1;
+            int k=n-1;
             while(j<k){
                 int sum=nums[i]+nums[j]+nums[k];
                 if(sum<0){
-                   j++;
+                    j++;
                 }
                 else if(sum>0){
                     k--;
                 }
                 else{
-                   vector<int>temp={nums[i],nums[j],nums[k]};
-                   ans.push_back(temp);
-                   j++;
-                   k--;
-                   while(j<k && nums[j]==nums[j-1]) j++;
-                   while(j<k && nums[k]==nums[k+1]) k--; 
+                    vector<int>temp={nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
                 }
             }
         }
