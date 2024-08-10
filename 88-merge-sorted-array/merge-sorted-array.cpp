e972df7d28db48feebb327e206ae893c3d118941
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-11 03:41:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (48.31%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         //brute force 
         // int i=m;
@@ -7,24 +8,22 @@ public:
         //     nums1[i] = nums2[j];
         //     i++;
         // }
-        // sort(nums1.begin(),nums1.end());gn
+        // sort(nums1.begin(),nums1.end());
 
         // time complexity O(m+n)+O(nlogn)
 
         //optimal solution
-        int i=m-1;
-        int j=n-1;
-        int k=m+n-1;
-
-        while(j>=0){
-            if(i>=0 && nums1[i]>nums2[j]){
-                nums1[k--]=nums1[i--];
-            }
-            else{
-                nums1[k--]=nums2[j--];
-            }
-        }
-
+       int i=m-1;
+       int j=n-1;
+       int k=m+n-1;
+       while(j>=0){
+          if(i>=0 && nums1[i]>nums2[j]){
+            nums1[k--]=nums1[i--];
+          }
+          else{
+            nums1[k--]=nums2[j--];
+          }
+       }
         // time complexity O(m+n)
     }
 };
