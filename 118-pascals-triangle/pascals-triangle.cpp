56419e6730596057445b1717f7f28554c12b22c0
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-11 01:25:27 +0530

    Time: 4 ms (33.51%) | Memory: 8.1 MB (41.97%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>r(numRows);
+        vector<vector<int>>temp(numRows);
 
         for(int i=0;i<numRows;i++){
-            r[i].resize(i+1);
-            r[i][0]=r[i][i]=1;
-
+            temp[i].resize(i+1);
+            temp[i][0]=temp[i][i]=1;
             for(int j=1;j<i;j++){
-                r[i][j]=r[i-1][j-1]+r[i-1][j];
+                temp[i][j]=temp[i-1][j-1]+temp[i-1][j];
             }
         }
-        return r;
-        //time complexity O(n^2)
-
-        // for calculating the value of a given position 
-        // use the formula NcK
-        // for(int i=0;i<k;i++){
-        //     res*=(n-i);
-        //     res/=(i+1);
-        // }
+        return temp;
     }
 };
