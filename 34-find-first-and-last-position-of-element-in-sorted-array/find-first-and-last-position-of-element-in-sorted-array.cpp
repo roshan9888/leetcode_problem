41cdc5098fa8e34e19b0e61e5e77e806559021d6
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-11 16:53:00 +0530

    Time: 3 ms (89.37%) | Memory: 16.3 MB (23.99%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int i=0;
-        int fo=-1;
-        int lo=-1;
-        while(i<nums.size()){
+        //brute force
+        // int i=0;
+        // int fo=-1;
+        // int lo=-1;
+        // while(i<nums.size()){
+        //     if(nums[i]==target){
+        //         fo=i;
+        //         lo=i;
+        //         break;
+        //     }
+        //     i++;
+        // }
+        // while(i<nums.size()){
+        //     if(nums[i]==target){
+        //         lo=i;
+        //     }
+        //     i++;
+        // }
+        // return {fo,lo};
+        
+        int fo=-1,lo=-1;
+        for(int i=0;i<nums.size();i++){
             if(nums[i]==target){
-                fo=i;
-                lo=i;
-                break;
-            }
-            i++;
-        }
-        while(i<nums.size()){
-            if(nums[i]==target){
+                if(fo==-1){
+                    fo=i;
+                }
                 lo=i;
             }
-            i++;
         }
         return {fo,lo};
+
+        //optimal solution
+
     }
 };
