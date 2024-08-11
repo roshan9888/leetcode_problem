98d0c172495c4b8edda49e3a86e295e2a62a8cf1
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-11 18:24:57 +0530

    Time: 18 ms (64.63%) | Memory: 25 MB (26.94%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -11,28 +11,28 @@ public:
         //optimal solution
         int n=nums.size();
         int low=1;
-        int high=nums.size()-2;
-        if(nums.size()==1) return nums[0];
+        int high=n-2;
+        if(n==1) return nums[0];
         if(nums[0]!=nums[1]) return nums[0];
         if(nums[n-1]!=nums[n-2]) return nums[n-1];
         while(low<=high){
             int mid=low+(high-low)/2;
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
             if(mid%2==0){
-               if(nums[mid]!=nums[mid+1]){
-                  high=mid-1;
-               }
-               else{
-                low=mid+1;
-               }
+                if(nums[mid]!=nums[mid+1]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
             }
             else{
-               if(nums[mid]!=nums[mid-1]){
-                 high=mid-1;
-               }
-               else{
-                low=mid+1;
-               }
+                if(nums[mid]!=nums[mid-1]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
             }
         }
         return -1;
