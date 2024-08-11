f4194e332e06130cf5b4bc0f3d2adf4a6a771ccc
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-12 01:18:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (33.19%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -35,6 +35,10 @@ public:
                 high=mid-1;
             }
         }
-        return k+high+1;
+        // cout<<arr[high]<<" ";
+        // int more=k-(arr[high]-(high+1));
+        // cout<<more<<" ";
+        // arr[high]+k-(arr[high]-(high+1))
+        return high+1+k;
     }
 };
