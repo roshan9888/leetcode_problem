56839b0e091cf98b3f350deddbcb12dda5d1c4c2
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-11 17:13:41 +0530

    Time: 2 ms (90.77%) | Memory: 16.4 MB (6.11%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,5 +1,35 @@
 class Solution {
-public:
+public: 
+    int lowerbound(vector<int>& nums,int low,int high,int target){
+        int ans=nums.size();
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+
+    int  upperbound(vector<int>& nums,int low,int high,int target){
+        int ans=nums.size();
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>target){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+
     vector<int> searchRange(vector<int>& nums, int target) {
         //brute force
         // int i=0;
@@ -21,18 +51,24 @@ public:
         // }
         // return {fo,lo};
         
-        int fo=-1,lo=-1;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                if(fo==-1){
-                    fo=i;
-                }
-                lo=i;
-            }
-        }
-        return {fo,lo};
+        // int fo=-1,lo=-1;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==target){
+        //         if(fo==-1){
+        //             fo=i;
+        //         }
+        //         lo=i;
+        //     }
+        // }
+        // return {fo,lo};
 
         //optimal solution
-
+        int n=nums.size();
+        int left=lowerbound(nums,0,n-1,target);
+        int right=upperbound(nums,0,n-1,target);
+        cout<<left<<" ";
+        cout<<right<<" ";
+        if(left==n || nums[left]!=target) return {-1,-1};
+        return {left,right-1};
     }
 };
