e1757a3920ccefe8a0f39c242c0814740ea5c420
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-12 04:53:46 +0530

    Time: 11 ms (98.69%) | Memory: 95.4 MB (30.42%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
-        int n1 = a.size(), n2 = b.size();
-    //if n1 is bigger swap the arrays:
-    if (n1 > n2) return findMedianSortedArrays(b, a);
-
-    int n = n1 + n2; //total length
-    int left = (n1 + n2 + 1) / 2; //length of left half
-    //apply binary search:
-    int low = 0, high = n1;
-    while (low <= high) {
-        int mid1 = (low + high) >> 1;
-        int mid2 = left - mid1;
-        //calculate l1, l2, r1 and r2;
-        int l1 = INT_MIN, l2 = INT_MIN;
-        int r1 = INT_MAX, r2 = INT_MAX;
-        if (mid1 < n1) r1 = a[mid1];
-        if (mid2 < n2) r2 = b[mid2];
-        if (mid1 - 1 >= 0) l1 = a[mid1 - 1];
-        if (mid2 - 1 >= 0) l2 = b[mid2 - 1];
-
-        if (l1 <= r2 && l2 <= r1) {
-            if (n % 2 == 1) return max(l1, l2);
-            else return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
+        int m=nums2.size();
+        vector<int>temp;
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(nums1[i]<nums2[j]){
+                temp.push_back(nums1[i++]);
+            }
+            else{
+                temp.push_back(nums2[j++]);
+            }
         }
 
-        //eliminate the halves:
-        else if (l1 > r2) high = mid1 - 1;
-        else low = mid1 + 1;
-    }
-    return 0; //dummy statement
+        while(i<n){
+            temp.push_back(nums1[i++]);   
+        }
+
+        while(j<m){
+            temp.push_back(nums2[j++]);
+        }
+
+        int t=temp.size();
+        if(t%2){
+           return temp[t/2];
+        }
+        return (temp[t / 2] + temp[t / 2 - 1]) / 2.0;
+        // return (double)(((double)temp[t/2]) + ((double)temp[t/2-1]))/2.0;
     }
 };
