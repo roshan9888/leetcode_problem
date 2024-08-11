94c6b2a269ed927419aa45b53ebe93e6e619ea22
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-12 05:12:19 +0530

    Time: 28 ms (41.90%) | Memory: 94.5 MB (74.41%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,32 +1,76 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m=nums2.size();
-        vector<int>temp;
-        int i=0,j=0;
-        while(i<n && j<m){
+        //brute force
+        // int n=nums1.size();
+        // int m=nums2.size();
+        // vector<int>temp;
+        // int i=0,j=0;
+        // while(i<n && j<m){
+        //     if(nums1[i]<nums2[j]){
+        //         temp.push_back(nums1[i++]);
+        //     }
+        //     else{
+        //         temp.push_back(nums2[j++]);
+        //     }
+        // }
+
+        // while(i<n){
+        //     temp.push_back(nums1[i++]);   
+        // }
+
+        // while(j<m){
+        //     temp.push_back(nums2[j++]);
+        // }
+
+        // int t=temp.size();
+        // if(t%2){
+        //    return temp[t/2];
+        // }
+        // return (temp[t / 2] + temp[t / 2 - 1]) / 2.0;
+
+        //optimal solution
+        int i=0;
+        int j=0;
+        int n=nums1.size()+nums2.size();
+        int ind2=n/2;
+        int ind1=ind2-1;
+        int cnt=0;
+        int ind1el=-1;
+        int ind2el=-1;
+        while(i<nums1.size() && j<nums2.size()){
             if(nums1[i]<nums2[j]){
-                temp.push_back(nums1[i++]);
+                if(cnt==ind1) ind1el=nums1[i];
+                if(cnt==ind2) ind2el=nums1[i];
+                cnt++;
+                i++;
             }
             else{
-                temp.push_back(nums2[j++]);
+                if(cnt==ind1) ind1el=nums2[j];
+                if(cnt==ind2) ind2el=nums2[j];
+                cnt++;
+                j++;
             }
         }
 
-        while(i<n){
-            temp.push_back(nums1[i++]);   
+        while(i<nums1.size()){
+            if(cnt==ind1) ind1el=nums1[i];
+            if(cnt==ind2) ind2el=nums1[i];
+            cnt++;
+            i++;
         }
 
-        while(j<m){
-            temp.push_back(nums2[j++]);
+        while(j<nums2.size()){
+            if(cnt==ind1) ind1el=nums2[j];
+            if(cnt==ind2) ind2el=nums2[j];
+            cnt++;
+            j++;
         }
 
-        int t=temp.size();
-        if(t%2){
-           return temp[t/2];
+        if(n%2){
+            return (double)ind2el;
         }
-        return (temp[t / 2] + temp[t / 2 - 1]) / 2.0;
-        // return (double)(((double)temp[t/2]) + ((double)temp[t/2-1]))/2.0;
+
+        return ((double)ind1el+(double)ind2el)/2.0;
     }
 };
