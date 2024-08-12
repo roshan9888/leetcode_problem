e1834331c97850e3e2fd742184d6f9f2db6d0212
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-13 04:27:00 +0530

    Time: 482 ms (57.37%) | Memory: 92.2 MB (58.86%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.cpp
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -1,39 +1,3 @@
-// class Solution {
-// public:
-//     void solve(int ind,string temp,long long sum,long long prev,string &num,int target,vector<string>&ans){
-//         if(ind==num.size()){
-//             if(sum==target){
-//                 ans.push_back(temp);
-//             }
-//             return;
-//         }
-
-//         for(int i=ind;i<num.size();i++){
-//             if(i>ind && num[ind]=='0'){
-//                 return;
-//             }
-//             long long number=stoll(num.substr(ind,i-ind+1));
-//             // cout<<number<<" ";
-//             if(ind==0){
-//                 solve(ind+1,temp+num,number,number,num,target,ans);
-//             }
-//             else{
-//                 solve(ind+1,temp+"+"+num.substr(ind,i-ind+1),sum+number,number,num,target,ans);
-//                 solve(ind+1,temp+"-"+num.substr(ind,i-ind+1),sum-number,-number,num,target,ans);
-//                 // solve(ind+1,temp+"*"+num.substr(ind,i-ind+1),(sum-prev+(prev*number)),(prev*number),num,target,ans);
-//                 solve(ind + 1, temp + "*" + num.substr(ind, i - ind + 1),
-//       sum - prev + (prev * number), (prev * number), num, target, ans);
-//             }
-//         }
-//     }
-
-//     vector<string> addOperators(string num, int target) {
-//         vector<string>ans;
-//         solve(0,"",0,0,num,target,ans);
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     void solve(int ind, string temp, long long sum, long long prev, string &num, int target, vector<string>& ans) {
