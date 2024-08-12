8f203981ec2788f4cd179fe7f9b9fb8c9b196363
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-12 19:44:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (22.40%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    bool search(vector<int>&nums,int target,int m){
-       int low=0;
-       int high=m-1;
-       cout<<m<<" ";
-       while(low<=high){
-           int mid=(low+high)/2;
-            if(nums[mid]==target) return true;
-           if(nums[mid]>target){
-               high=mid-1;
-           }
-           else{
-               low=mid+1;
-           }
-       }
-       return false;
+    bool find(vector<int>&arr,int target,int m){
+        int low=0;
+        int high=m-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==target) return true;
+
+            if(arr[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return false;
     }
+
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m=matrix[0].size();
-        bool ans;
+        bool ans=false;
         for(int i=0;i<n;i++){
-            ans=search(matrix[i],target,m);
-            if(ans){
-                return ans;
+            if(find(matrix[i],target,m)){
+                return true;
             }
         }
-        return ans;
+        return false;
     }
 };
