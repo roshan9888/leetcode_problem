81649f8cdb9d8427d79f26d0c995a66efdb03269
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-13 04:24:42 +0530

    Time: 484 ms (56.69%) | Memory: 92.3 MB (56.28%) - LeetSync

---
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -0,0 +1,72 @@
+// class Solution {
+// public:
+//     void solve(int ind,string temp,long long sum,long long prev,string &num,int target,vector<string>&ans){
+//         if(ind==num.size()){
+//             if(sum==target){
+//                 ans.push_back(temp);
+//             }
+//             return;
+//         }
+
+//         for(int i=ind;i<num.size();i++){
+//             if(i>ind && num[ind]=='0'){
+//                 return;
+//             }
+//             long long number=stoll(num.substr(ind,i-ind+1));
+//             // cout<<number<<" ";
+//             if(ind==0){
+//                 solve(ind+1,temp+num,number,number,num,target,ans);
+//             }
+//             else{
+//                 solve(ind+1,temp+"+"+num.substr(ind,i-ind+1),sum+number,number,num,target,ans);
+//                 solve(ind+1,temp+"-"+num.substr(ind,i-ind+1),sum-number,-number,num,target,ans);
+//                 // solve(ind+1,temp+"*"+num.substr(ind,i-ind+1),(sum-prev+(prev*number)),(prev*number),num,target,ans);
+//                 solve(ind + 1, temp + "*" + num.substr(ind, i - ind + 1),
+//       sum - prev + (prev * number), (prev * number), num, target, ans);
+//             }
+//         }
+//     }
+
+//     vector<string> addOperators(string num, int target) {
+//         vector<string>ans;
+//         solve(0,"",0,0,num,target,ans);
+//         return ans;
+//     }
+// };
+
+class Solution {
+public:
+    void solve(int ind, string temp, long long sum, long long prev, string &num, int target, vector<string>& ans) {
+        if (ind == num.size()) {
+            if (sum == target) {
+                ans.push_back(temp);
+            }
+            return; // Ensure the function returns after checking the base case
+        }
+
+        for (int i = ind; i < num.size(); i++) {
+            // Avoid numbers with leading zeros
+            if (i > ind && num[ind] == '0') {
+                break; // Use break to skip further processing in this loop
+            }
+            long long number = stoll(num.substr(ind, i - ind + 1));
+            if (ind == 0) {
+                // Initial case with no operator
+                solve(i + 1, temp + num.substr(ind, i - ind + 1), number, number, num, target, ans);
+            } else {
+                // Addition
+                solve(i + 1, temp + "+" + num.substr(ind, i - ind + 1), sum + number, number, num, target, ans);
+                // Subtraction
+                solve(i + 1, temp + "-" + num.substr(ind, i - ind + 1), sum - number, -number, num, target, ans);
+                // Multiplication
+                solve(i + 1, temp + "*" + num.substr(ind, i - ind + 1), sum - prev + (prev * number), prev * number, num, target, ans);
+            }
+        }
+    }
+
+    vector<string> addOperators(string num, int target) {
+        vector<string> ans;
+        solve(0, "", 0, 0, num, target, ans);
+        return ans;
+    }
+};
