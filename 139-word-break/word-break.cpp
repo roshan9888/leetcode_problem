372c5b1553b28c93597c3e8a4ecee386a8146d12
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-13 03:42:24 +0530

    Time: 28 ms (14.23%) | Memory: 22.9 MB (10.54%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
-    bool solve(int ind,string s,unordered_set<string>&mp,vector<int>&dp){
+    bool solve(int ind,string s,unordered_set<string>&mp){
+        if(ind>=s.size()){
+            return true;
+        }
+
+        if(mp.find(s)!=mp.end()){
+            return true;
+        }
+        for(int l=1;l<=s.size();l++){
+            string temp=s.substr(ind,l);
+            if(mp.find(temp)!=mp.end() && solve(ind+l,s,mp)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool topdown(int ind,string s,unordered_set<string>&mp,vector<int>&dp){
         if(ind>=s.size()){
             return true;
         }
@@ -11,21 +28,25 @@ public:
         if(mp.find(s)!=mp.end()) return true;
         for(int l=1;l<=s.size();l++){
             string temp=s.substr(ind,l);
-            if(mp.find(temp)!=mp.end() && solve(ind+l,s,mp,dp)){
+            if(mp.find(temp)!=mp.end() && topdown(ind+l,s,mp,dp)){
                 return dp[ind]=true;
             }
         }
         return dp[ind]=false;
     }
+
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string>mp;
         for(auto it:wordDict){
             mp.insert(it);
         }
+        // return solve(0,s,mp);
         // for(auto it:mp){
         //     cout<<it<<" ";
         // }
+
+        //top down dp
         vector<int>dp(s.size(),-1);
-        return solve(0,s,mp,dp);
+        return topdown(0,s,mp,dp);
     }
 };
