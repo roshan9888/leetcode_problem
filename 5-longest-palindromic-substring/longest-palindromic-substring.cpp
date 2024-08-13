2ef901a8cbaa585b9165c92962f2e2ba75b2993d
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-14 00:47:09 +0530

    Time: 1318 ms (6.52%) | Memory: 501.7 MB (5.37%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,16 +1,45 @@
 class Solution {
 public:
-    bool isPalindrome(string &s,int start,int end){
-        if(start>=end){
-            return true;
+    // bool isPalindrome(string &s,int start,int end){
+    //     if(start>=end){
+    //         return true;
+    //     }
+
+    //     if(s[start]!=s[end]){
+    //         return false;
+    //     }
+        
+    //     return isPalindrome(s,start+1,end-1);;
+    // } 
+
+    bool isPalindrome(string &s,int start,int end,vector<vector<bool>> &dp){
+        return dp[start][end];
+    }
+     
+    void buildDPTable(const string &s, vector<vector<bool>> &dp) {
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            dp[i][i] = true;
         }
 
-        if(s[start]!=s[end]){
-            return false;
+        // Initialize two character substrings
+        for (int i = 0; i < n - 1; ++i) {
+            dp[i][i + 1] = (s[i] == s[i + 1]);
         }
-        
-        return isPalindrome(s,start+1,end-1);;
+
+        // Fill the dp table for substrings longer than 2
+        for (int len = 3; len <= n; ++len) { // len is the length of the substring
+            for (int i = 0; i < n - len + 1; ++i) {
+                int j = i + len - 1; // Ending index of the substring
+
+                // Check if s[i...j] is a palindrome
+                if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
     }
+
 
     // void solve(string s,int ind,string&current,string&longest){
     //     if(ind>=s.size()){
@@ -34,10 +63,13 @@ public:
         // solve(s,0,current,longest);  
 
         //optimize
+        int n=s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        buildDPTable(s,dp);
         string longest="";
         for(int i=0;i<s.size();i++){
             for(int j=i;j<s.size();j++){
-                if(isPalindrome(s,i,j)){
+                if(isPalindrome(s,i,j,dp)){
                     string curr=s.substr(i,j-i+1);
                     if(longest.size()<curr.size()){
                         longest=curr;
