dfdfe700072e7cc47b5cfad2bc6ec00c7e2577f1
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-13 19:14:28 +0530

    Time: 2 ms (70.41%) | Memory: 8 MB (63.40%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,34 +1,54 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        stack<char>st;
-        vector<string>v;
-        string temp="";
-        for(int i=0;i<s.size();i++){
-            if(st.empty()){
-                st.push(s[i]);
-                temp+=s[i];
-            }
-            else if(st.top()=='(' && s[i]==')'){
-                temp+=s[i];
-                st.pop();
-            }
-            else{
-                temp+=s[i];
-                st.push(s[i]);
-            }
-            if(st.empty()){
-                v.push_back(temp);
-                temp="";
+    //     stack<char>st;
+    //     vector<string>v;
+    //     string temp="";
+    //     for(int i=0;i<s.size();i++){
+    //         if(st.empty()){
+    //             st.push(s[i]);
+    //             temp+=s[i];
+    //         }
+    //         else if(st.top()=='(' && s[i]==')'){
+    //             temp+=s[i];
+    //             st.pop();
+    //         }
+    //         else{
+    //             temp+=s[i];
+    //             st.push(s[i]);
+    //         }
+    //         if(st.empty()){
+    //             v.push_back(temp);
+    //             temp="";
+    //         }
+    //     }
+    //     string sub="";
+    //     for (const auto& str : v) {
+    //     if (str.length() >= 2) {  // Ensure the string has at least two characters
+    //         sub += str.substr(1, str.length() - 2);
+    //         // cout << "Substring without first and last character of " << str << ": " << sub << endl;
+    //     } 
+    // }
+    //     return sub;
+
+    //optimal solution
+    string result = "";
+        int level = 0;
+        
+        for (char c : s) {
+            if (c == '(') {
+                if (level > 0) {
+                    result += c;
+                }
+                level++;
+            } else if (c == ')') {
+                level--;
+                if (level > 0) {
+                    result += c;
+                }
             }
         }
-        string sub="";
-        for (const auto& str : v) {
-        if (str.length() >= 2) {  // Ensure the string has at least two characters
-            sub += str.substr(1, str.length() - 2);
-            // cout << "Substring without first and last character of " << str << ": " << sub << endl;
-        } 
-    }
-        return sub;
+        
+        return result;
     }
 };
