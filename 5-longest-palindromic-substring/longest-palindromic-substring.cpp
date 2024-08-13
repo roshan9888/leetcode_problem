152d12b157cdb406f2ab4b0f6ff4f8b5ad31731c
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-14 00:12:21 +0530

    Time: 1715 ms (5.00%) | Memory: 491.9 MB (5.37%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,25 +1,50 @@
 class Solution {
 public:
-bool isPalindrome(string &s,int start ,int end){
-        while(start<end){
-            if(s[start]!=s[end]){
-                return false;
-            }
-            start++;
-            end--;
+    bool isPalindrome(string &s,int start,int end){
+        if(start>=end){
+            return true;
         }
-        return true;
+
+        if(s[start]!=s[end]){
+            return false;
+        }
+        
+        return isPalindrome(s,start+1,end-1);;
     }
+
+    // void solve(string s,int ind,string&current,string&longest){
+    //     if(ind>=s.size()){
+    //         return;
+    //     }
+
+    //     for(int i=ind;i<s.size();i++){
+    //         if(isPalindrome(s,ind,i)){
+    //            string can=s.substr(ind,i-ind+1);
+    //            if(can.size()>longest.size()){
+    //              longest=can;
+    //            }
+    //            solve(s,i+1,current,longest);
+    //         }
+    //     }
+    // }
+
     string longestPalindrome(string s) {
-        string ans="";
+        // string longest;
+        // string current;
+        // solve(s,0,current,longest);  
+
+        //optimize
+        string longest="";
         for(int i=0;i<s.size();i++){
             for(int j=i;j<s.size();j++){
                 if(isPalindrome(s,i,j)){
-                    string t=s.substr(i,j-i+1);
-                    ans=t.size()>ans.size()?t:ans;
+                    string curr=s.substr(i,j-i+1);
+                    if(longest.size()<curr.size()){
+                        longest=curr;
+                    }
                 }
             }
         }
-        return ans;
+        return longest;
     }
 };
