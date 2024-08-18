329c57c8fa7e1ff312b1834f337bab927b16858f
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-08-19 00:37:15 +0530

    Time: 519 ms (79.81%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,16 +1,23 @@
 # Write your MySQL query statement below
 
-select coalesce(round(sum(1)/(select count(distinct player_id) from activity),2) ,0)as fraction
-from activity a1
-join activity a2
-on a1.player_id=a2.player_id
-and date_sub(a1.event_date,interval 1 day)=a2.event_date
-where (a2.player_id, a2.event_date )
-in
-(select
-    player_id, min(event_date)
-    from
-    activity
-    group by player_id
-)
-;
+-- select coalesce(round(sum(1)/(select count(distinct player_id) from activity),2) ,0)as fraction
+-- from activity a1
+-- join activity a2
+-- on a1.player_id=a2.player_id
+-- and date_sub(a1.event_date,interval 1 day)=a2.event_date
+-- where (a2.player_id, a2.event_date )
+-- in
+-- (select
+--     player_id, min(event_date)
+--     from
+--     activity
+--     group by player_id
+-- )
+-- ;
+
+select round(count(distinct player_id)/(select count(distinct player_id) from activity),2) as fraction 
+from activity
+where (player_id,date_sub(event_date,interval 1 day)) in
+(select player_id,min(event_date) as first_login
+from activity
+group by player_id)
