201b16788559f104097bdbb30e25885634f7a5bb
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-09-01 05:55:43 +0530

    Time: 8 ms (31.70%) | Memory: 12.4 MB (21.90%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -31,21 +31,38 @@ public:
     // }
 
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans;
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums2.size();i++){
-            mp[nums2[i]]++;
-        }        
+        // vector<int>ans;
+        // unordered_map<int,int>mp;
+        // for(int i=0;i<nums2.size();i++){
+        //     mp[nums2[i]]++;
+        // }        
 
-        for(int i=0;i<nums1.size();i++){
-            if(mp[nums1[i]]){
-                ans.push_back(greater(nums1[i],nums2));
-            }
-        }
-        return ans;
+        // for(int i=0;i<nums1.size();i++){
+        //     if(mp[nums1[i]]){
+        //         ans.push_back(greater(nums1[i],nums2));
+        //     }
+        // }
+        // return ans;
 
         //using stack O(n) for a circular array
         // vector<int>ans=nextgreaterelement(nums2);
         // return ans;
+
+        //optimal 
+        unordered_map<int,int>nge;
+        vector<int>ans;
+        stack<int>st;
+        for(int i=nums2.size()-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums2[i]){
+                st.pop();
+            }
+            nge[nums2[i]]=st.empty()?-1:st.top();
+            st.push(nums2[i]);
+        }
+
+        for(auto it:nums1){
+            ans.push_back(nge[it]);
+        }
+        return ans;
     }
 };
