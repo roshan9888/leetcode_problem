159eb08a490753d54cadd96de53e66b91309bdc2
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-09-18 13:43:10 +0530

    Time: 6 ms (80.28%) | Memory: 10.1 MB (17.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int>mp;
+
         if(s.size()!=t.size()) return false;
+        unordered_map<char,int>mp;
         for(int i=0;i<s.size();i++){
-              mp[s[i]]++;
+            mp[s[i]]++;
         }
+
         for(int i=0;i<t.size();i++){
-            mp[t[i]]--;
+            if(mp[t[i]]){
+                mp[t[i]]--;
+            }
         }
-        cout<<mp.size()<<" ";
+
         for(auto it:mp){
             if(it.second>0){
                 return false;
             }
         }
+
         return true;
     }
 };
