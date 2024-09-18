094528f2548841b2b7bd32ec05e7760b4c0b7894
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-09-18 12:18:57 +0530

    Time: 6 ms (46.06%) | Memory: 8.7 MB (75.46%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,44 +1,67 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stack<char>st;
-        stack<string>ans;
-        for(int i=0;i<s.size();i++){
-            string temp="";
-            if(s[i]!=' '){
-                st.push(s[i]);
+        // stack<char>st;
+        // stack<string>ans;
+        // for(int i=0;i<s.size();i++){
+        //     string temp="";
+        //     if(s[i]!=' '){
+        //         st.push(s[i]);
+        //     }
+        //     else{
+        //         while(!st.empty()){
+        //             temp.push_back(st.top());
+        //             st.pop();
+        //         }
+
+        //         if(temp.size()!=0){
+        //             reverse(temp.begin(),temp.end());
+        //             ans.push(temp);
+        //         }
+        //     }
+        // }
+        // string temp="";
+        // while(!st.empty()){
+        //     temp.push_back(st.top());
+        //     st.pop();
+        // }
+
+        // if(temp.size()!=0){
+        //     reverse(temp.begin(),temp.end());
+        //     ans.push(temp);
+        // }
+
+        // string finalans="";
+        // while(!ans.empty()){
+        //     finalans+=ans.top();
+        //     finalans+=" ";
+        //     ans.pop();
+        // }
+
+        // finalans=finalans.substr(0,finalans.length()-1);
+        // return finalans;
+
+        //optimize
+        int l=0,r=0,i=0;
+        reverse(s.begin(),s.end());
+        while(i<s.size()){
+            while(i<s.size() && s[i]!=' '){
+                s[r++]=s[i++];
             }
-            else{
-                while(!st.empty()){
-                    temp.push_back(st.top());
-                    st.pop();
-                }
 
-                if(temp.size()!=0){
-                    reverse(temp.begin(),temp.end());
-                    ans.push(temp);
-                }
+            if(l<r){
+                reverse(s.begin()+l,s.begin()+r);
+
+                s[r]=' ';
+                r++;
+
+                l=r;
             }
-        }
-        string temp="";
-        while(!st.empty()){
-            temp.push_back(st.top());
-            st.pop();
+
+            i++;
         }
 
-        if(temp.size()!=0){
-            reverse(temp.begin(),temp.end());
-            ans.push(temp);
-        }
-
-        string finalans="";
-        while(!ans.empty()){
-            finalans+=ans.top();
-            finalans+=" ";
-            ans.pop();
-        }
-
-        finalans=finalans.substr(0,finalans.length()-1);
-        return finalans;
+        string str=s.substr(0,r-1);
+        return str;
     }
 };
