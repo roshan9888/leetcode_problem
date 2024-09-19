cfa506ebca323c075fa53aec5343c289bb27d6ef
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-09-19 09:58:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (19.41%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -13,12 +13,12 @@ public:
     ListNode* middleNode(ListNode* head) {
         ListNode*slow=head;
         ListNode*fast=head;
-        while(fast!=NULL){
-            fast=fast->next;
-            if(fast!=NULL){
+        while(fast->next!=NULL){
             slow=slow->next;
             fast=fast->next;
-            } 
+            if(fast->next!=NULL){
+                fast=fast->next;
+            }
         }
         return slow;
     }
