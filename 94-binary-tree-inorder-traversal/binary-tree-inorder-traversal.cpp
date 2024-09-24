302a4db7c400a0f067f3fb655edf70cb2854ce2f
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-09-24 10:50:51 +0530

    Time: 4 ms (32.35%) | Memory: 9.9 MB (88.11%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,75 +11,16 @@
  */
 class Solution {
 public:
-    // void inorder(TreeNode*root,vector<int>&ans){
-    //     if(root==NULL) return;
-    //     inorder(root->left,ans);
-    //     ans.push_back(root->val);
-    //     inorder(root->right,ans);
-    // }
-
-    // void MorrisTraversalInorder(TreeNode*root,vector<int>&inorder){
-    //     TreeNode*cur=root;
-    //     while(cur!=NULL){
-    //         if(cur->left==NULL){
-    //             inorder.push_back(cur->val);
-    //             cur=cur->right;
-    //         }
-    //         else{
-    //            TreeNode*prev=cur->left;
-    //            while(prev->right && prev->right!=cur){
-    //               prev=prev->right;
-    //            }
-
-    //            if(prev->right==NULL){
-    //               prev->right=cur;
-    //               cur=cur->left;
-    //            }
-    //            else{
-    //             prev->right=NULL;
-    //             inorder.push_back(cur->val);
-    //             cur=cur->right;
-    //            }
-    //         }
-    //     }
-    // }
-
-    void MorrisTraversalPreorder(TreeNode*root,vector<int>&Preorder){
-        TreeNode*cur=root;
-        while(cur!=NULL){
-            if(cur->left==NULL){
-                Preorder.push_back(cur->val);
-                cur=cur->right;
-            }
-            else{
-               TreeNode*prev=cur->left;
-               while(prev->right && prev->right!=cur){
-                  prev=prev->right;
-               }
-               if(prev->right==cur){
-                prev->right=NULL;
-                Preorder.push_back(cur->val);
-                cur=cur->right;
-               }
-               else{
-                prev->right=cur;
-                cur=cur->left;
-               }
-            }
-        }
+    void inorder(vector<int>&ans,TreeNode*root){
+        if(root==NULL) return;
+        inorder(ans,root->left);
+        ans.push_back(root->val);
+        inorder(ans,root->right);
     }
 
     vector<int> inorderTraversal(TreeNode* root) {
-        // vector<int>ans;
-        // inorder(root,ans);
-        // return ans;
-
-        // vector<int>inorder;
-        // MorrisTraversalInorder(root,inorder);
-        // return inorder;
-
-        vector<int>Preorder;
-        MorrisTraversalPreorder(root,Preorder);
-        return Preorder;
+        vector<int>ans;
+        inorder(ans,root);
+        return ans;
     }
 };
