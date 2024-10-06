65aaa1542a5132c6803c505d9709610e1ad6817d
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-10-06 22:14:49 +0530

    Time: 1000 ms (34.69%) | Memory: 12.1 MB (18.04%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    bool search(string word,int ind,vector<vector<char>>&board,int i,int j){
-        if(ind==word.size()){
-            return true;
-        }
+    bool solve(vector<vector<char>>&board,int ind,string word,int i,int j){
+        if(ind==word.size()) return true;
 
         if(i<0 || j<0 || i==board.size() || j==board[0].size() || board[i][j]!=word[ind]){
             return false;
         }
         bool a,b,c,d;
         board[i][j]='$';
-        a=search(word,ind+1,board,i+1,j);
-        b=search(word,ind+1,board,i-1,j);
-        c=search(word,ind+1,board,i,j+1);
-        d=search(word,ind+1,board,i,j-1);
+        a=solve(board,ind+1,word,i+1,j);
+        b=solve(board,ind+1,word,i-1,j);
+        c=solve(board,ind+1,word,i,j+1);
+        d=solve(board,ind+1,word,i,j-1);
         board[i][j]=word[ind];
-
         if(a || b || c || d) return true;
+
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        for(int i=0;i<board.size();i++){
+        int n=board.size();
+        for(int i=0;i<n;i++){
             for(int j=0;j<board[i].size();j++){
                 bool x=false;
                 if(board[i][j]==word[0]){
-                    x=search(word,0,board,i,j);
+                    x=solve(board,0,word,i,j);
                 }
                 if(x) return true;
             }
