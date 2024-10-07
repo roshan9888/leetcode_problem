465123e35fb18ce375ec7ed431e49bfd2368abc2
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2024-10-08 05:07:32 +0530

    Time: 4 ms (85.64%) | Memory: 18.7 MB (21.91%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -9,44 +9,6 @@
 //  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 //  * };
 //  */
-// class Solution {
-// public:
-//     int widthOfBinaryTree(TreeNode* root) {
-//         if(!root) return 0;
-//         int ans=0;
-//         queue<pair<TreeNode*,int>>q;
-//         q.push({root,0});
-//         while(!q.empty()){
-//            int mmin = q.front().second;
-//            int first,last;
-//            int size=q.size();
-//            for(int i=0;i<size;i++){
-//               int curr_i=q.front().second-mmin;
-//               TreeNode* node=q.front().first;
-//               q.pop();
-
-//               if(i==0){
-//                 first=curr_i;
-//               }
-
-//               if(i==size-1){
-//                 last=curr_i;
-//               }
-
-//               if(node->left){
-//                 q.push({node->left,curr_i*2+1});
-//               }
-
-//               if(node->right){
-//                 q.push({node->right,curr_i*2+2});
-//               }
-//            }
-//            ans=max(ans,last-first+1);
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
