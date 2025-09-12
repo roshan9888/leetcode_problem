6a38b7d54b81cdd80023ea2a7a988f78eec09b47
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2025-09-12 10:12:33 +0530

    Time: 308 ms (57.87%) | Memory: 298.5 MB (64.85%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        unordered_map<int, vector<int>> graph;
-        for (const auto& edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            graph[u].push_back(v);
-            graph[v].push_back(u);
-        }
-        
-        unordered_set<int> visited;
-        return dfs(source, destination, graph, visited);
-    }
-    
-    bool dfs(int node, int destination, unordered_map<int, vector<int>>& graph, unordered_set<int>& visited) {
-        if (node == destination) {
-            return true;
-        }
-        visited.insert(node);
-        for (int neighbor : graph[node]) {
-            if (visited.find(neighbor) == visited.end()) {
-                if (dfs(neighbor, destination, graph, visited)) {
+    bool dfs(vector<vector<int>>&adj,int source,int destination,vector<bool>&visited){
+        if(source==destination) return true;
+        visited[source]=true;
+        for(int n:adj[source]){
+            if(!visited[n]){
+                if(dfs(adj,n,destination,visited)){
                     return true;
                 }
             }
         }
         return false;
+    }
+
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        vector<bool>visited(n,false);
+        vector<vector<int>>adj(n);
+        for(auto&e:edges){
+           adj[e[0]].push_back(e[1]);
+           adj[e[1]].push_back(e[0]);
+        }
+        return dfs(adj,source,destination,visited);
     }
 };
