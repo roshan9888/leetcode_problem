66f82b30c9a33b2e105ca1d06a4a9713cfbdbac1
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2025-09-13 11:21:27 +0530

    Time: 11 ms (21.52%) | Memory: 19.3 MB (74.89%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,38 +1,55 @@
 class Solution {
 public:
+    // bool dfs(int n,vector<vector<int>>&adj,vector<bool>&v,int s){
+    //     v[s]=true;
+
+    // }
+    bool dfs(int node, vector<vector<int>>& adj, vector<int>& visited) {
+    // 0 = not visited, 1 = visiting, 2 = visited
+    visited[node] = 1; // mark as visiting
+
+    for (int nei : adj[node]) {
+        if (visited[nei] == 0) {
+            if (dfs(nei, adj, visited)) return true; // cycle found
+        } 
+        else if (visited[nei] == 1) {
+            // back edge → cycle detected
+            return true;
+        }
+    }
+
+    visited[node] = 2; // mark as fully visited
+    return false;
+}
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>adj(numCourses);
-        for (const auto& prereq : prerequisites) {
-            adj[prereq[1]].push_back(prereq[0]);
-        }
-       
-        vector<int>indegree(numCourses,0);
-        for(int i=0;i<numCourses;i++){
-            for(auto it:adj[i]){
-                indegree[it]++;
-            }
-        }
-        queue<int>q;
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
-        }
+        // vector<bool>visited(numCourses,0);
+        // vector<vector<int>>adj(numCourses);
+        // for(auto e:numCourses){
+        //     adj[e[0]].push_back(e[1]);
+        // }
+        // for(int i=0;i<numCourses;i++){
+        //     if(visited[i]==0){
+        //         if(dfs(i,adj,visited)){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return dfs(numCourses,adj,visited,0);
+    int n=numCourses;
+    vector<vector<int>> adj(n);
+    for (auto& e : prerequisites) {
+        adj[e[0]].push_back(e[1]);
+    }
 
-        vector<int>topo;
-        while(!q.empty()){
-            int num=q.front();
-            q.pop();
-            topo.push_back(num);
-
-            for(auto it:adj[num]){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
-                }
+    vector<int> visited(n, 0); 
+    for (int i = 0; i < n; i++) {
+        if (visited[i] == 0) {
+            if (dfs(i, adj, visited)) {
+                return false; // cycle → not possible
             }
         }
-        if(topo.size()==numCourses) return true;
-        return false;
+    }
+
+    return true; // no cycle → possible
     }
 };
