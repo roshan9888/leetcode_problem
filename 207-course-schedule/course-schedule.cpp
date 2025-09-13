13653c228c5ad81daf85aed7b6a280ff723f8199
ROSHAN KUMAR <100696071+roshan9888@users.noreply.github.com>
2025-09-13 11:29:15 +0530

    Time: 2 ms (83.36%) | Memory: 19.1 MB (94.25%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
-    // bool dfs(int n,vector<vector<int>>&adj,vector<bool>&v,int s){
-    //     v[s]=true;
-
-    // }
     bool dfs(int node, vector<vector<int>>& adj, vector<int>& visited) {
-    // 0 = not visited, 1 = visiting, 2 = visited
-    visited[node] = 1; // mark as visiting
+    visited[node] = 1; 
 
     for (int nei : adj[node]) {
         if (visited[nei] == 0) {
-            if (dfs(nei, adj, visited)) return true; // cycle found
+            if (dfs(nei, adj, visited)) return true; 
         } 
         else if (visited[nei] == 1) {
-            // back edge → cycle detected
             return true;
         }
     }
 
-    visited[node] = 2; // mark as fully visited
+    visited[node] = 2; 
     return false;
 }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // vector<bool>visited(numCourses,0);
-        // vector<vector<int>>adj(numCourses);
-        // for(auto e:numCourses){
-        //     adj[e[0]].push_back(e[1]);
-        // }
-        // for(int i=0;i<numCourses;i++){
-        //     if(visited[i]==0){
-        //         if(dfs(i,adj,visited)){
-        //             return true;
-        //         }
-        //     }
-        // }
-        // return dfs(numCourses,adj,visited,0);
     int n=numCourses;
     vector<vector<int>> adj(n);
     for (auto& e : prerequisites) {
@@ -45,11 +26,11 @@ public:
     for (int i = 0; i < n; i++) {
         if (visited[i] == 0) {
             if (dfs(i, adj, visited)) {
-                return false; // cycle → not possible
+                return false; 
             }
         }
     }
 
-    return true; // no cycle → possible
+    return true; 
     }
 };
